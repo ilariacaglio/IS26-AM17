d037e8d6bf2b18727fdb9be144d5e5f674e578a5
MarcoCR04 <marco.crippa@issgreppi.it>
2026-03-12 16:20:12 +0100

    added Deck

---
+++ src/main/java/it/polimi/ingsw/am17/Model/Deck.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.am17.Model;
+
+import it.polimi.ingsw.am17.Model.GameCard.GameCard;
+import it.polimi.ingsw.am17.Model.GameCard.TribesCard;
+
+import java.util.List;
+
+public class Deck {
+    private List<TribesCard> tribesCards;
+    public GameCard Draw(){
+        return null;
+    }
+}
