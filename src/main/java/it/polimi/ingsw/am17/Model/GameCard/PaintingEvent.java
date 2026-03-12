5a4d95902133392cfd2e40d3699ae67757cc1f4d
MarcoCR04 <marco.crippa@issgreppi.it>
2026-03-12 16:16:40 +0100

    added PaintingEvent

---
+++ src/main/java/it/polimi/ingsw/am17/Model/GameCard/PaintingEvent.java
@@ -0,0 +1,24 @@
+package it.polimi.ingsw.am17.Model.GameCard;
+
+public class PaintingEvent extends EventCard {
+    private int pointsLow;
+    private int pointsMax;
+    private int numLow;
+    private int numMax;
+
+    public int getNumLow() {
+        return numLow;
+    }
+
+    public int getNumMax() {
+        return numMax;
+    }
+
+    public int getPointsLow() {
+        return pointsLow;
+    }
+
+    public int getPointsMax() {
+        return pointsMax;
+    }
+}
