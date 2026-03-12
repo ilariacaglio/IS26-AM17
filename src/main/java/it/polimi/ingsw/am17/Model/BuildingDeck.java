b0031cc213ac0a716a5e6e764aecd9bcd4801ffd
MarcoCR04 <marco.crippa@issgreppi.it>
2026-03-12 16:18:08 +0100

    added BuildingDeck

---
+++ src/main/java/it/polimi/ingsw/am17/Model/BuildingDeck.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.am17.Model;
+
+import it.polimi.ingsw.am17.Model.GameCard.BuildingCard;
+import it.polimi.ingsw.am17.Model.GameCard.GameCard;
+
+import java.util.List;
+
+public class BuildingDeck {
+    private List<BuildingCard> buildingCards;
+    public GameCard draw(){
+        return null;
+    }
+}
