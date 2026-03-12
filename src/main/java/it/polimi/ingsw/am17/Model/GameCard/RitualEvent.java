e3018f2fc756fde4977bdf97ff464be0e6e6a8cd
MarcoCR04 <marco.crippa@issgreppi.it>
2026-03-12 16:16:23 +0100

    added RitualEvent

---
+++ src/main/java/it/polimi/ingsw/am17/Model/GameCard/RitualEvent.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.am17.Model.GameCard;
+
+public class RitualEvent extends EventCard {
+    private int pointMax;
+    private int pointMin;
+
+    public int getPointMax() {
+        return pointMax;
+    }
+
+    public int getPointMin() {
+        return pointMin;
+    }
+}
