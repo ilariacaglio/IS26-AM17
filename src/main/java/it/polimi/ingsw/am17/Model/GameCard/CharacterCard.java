729092ede79a1ab0f7234de3ddc63e5a53a503c1
MarcoCR04 <marco.crippa@issgreppi.it>
2026-03-12 16:12:55 +0100

    added CharacterCard

---
+++ src/main/java/it/polimi/ingsw/am17/Model/GameCard/CharacterCard.java
@@ -0,0 +1,9 @@
+package it.polimi.ingsw.am17.Model.GameCard;
+
+public class CharacterCard extends TribesCard {
+    private int minPlayer;
+
+    public int getMinPlayer() {
+        return minPlayer;
+    }
+}
