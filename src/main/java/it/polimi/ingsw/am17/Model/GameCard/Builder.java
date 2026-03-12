c8b83320c7c60d9b2c57664ee464fae5f14820aa
MarcoCR04 <marco.crippa@issgreppi.it>
2026-03-12 16:14:02 +0100

    added Builder

---
+++ src/main/java/it/polimi/ingsw/am17/Model/GameCard/Builder.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.am17.Model.GameCard;
+
+public class Builder extends CharacterCard {
+    private int pointBonus;
+    private int foodReduction;
+
+    public int getFoodReduction() {
+        return foodReduction;
+    }
+
+    public int getPointBonus() {
+        return pointBonus;
+    }
+}
