c2bf54d9d6896ea6560a045787617e7f7fa817ef
MarcoCR04 <marco.crippa@issgreppi.it>
2026-03-12 16:19:14 +0100

    added CardRow

---
+++ src/main/java/it/polimi/ingsw/am17/Model/CardRow.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.am17.Model;
+
+import it.polimi.ingsw.am17.Model.GameCard.GameCard;
+import java.util.List;
+
+public class CardRow
+{
+    private List<GameCard> cardList;
+    public void addCard(GameCard card){}
+
+    public void showCards()
+    {}
+}
