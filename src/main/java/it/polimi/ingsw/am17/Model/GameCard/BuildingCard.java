2d2a8383db1092e0f8ee3da05a8b882801eb7333
MarcoCR04 <marco.crippa@issgreppi.it>
2026-03-12 16:17:45 +0100

    added BuildingCard

---
+++ src/main/java/it/polimi/ingsw/am17/Model/GameCard/BuildingCard.java
@@ -0,0 +1,9 @@
+package it.polimi.ingsw.am17.Model.GameCard;
+
+public class BuildingCard extends GameCard{
+    private int foodCost;
+    private int bonusPoints;
+
+
+    public void buyBuilding(){}
+}
