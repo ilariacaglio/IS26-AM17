b846cfbf758734fb106663c7d7e31ccb2fc24be0
MarcoCR04 <marco.crippa@issgreppi.it>
2026-03-12 16:16:57 +0100

    added HuntingEvent

---
+++ src/main/java/it/polimi/ingsw/am17/Model/GameCard/HuntingEvent.java
@@ -0,0 +1,9 @@
+package it.polimi.ingsw.am17.Model.GameCard;
+
+public class HuntingEvent extends EventCard {
+    private int pointEarned;
+
+    public int getPointEarned() {
+        return pointEarned;
+    }
+}
