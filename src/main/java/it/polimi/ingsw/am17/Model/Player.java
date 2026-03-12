3339643f40be54fa238615972a6527467baf4540
MarcoCR04 <marco.crippa@issgreppi.it>
2026-03-12 16:21:24 +0100

    added Player

---
+++ src/main/java/it/polimi/ingsw/am17/Model/Player.java
@@ -0,0 +1,75 @@
+package it.polimi.ingsw.am17.Model;
+
+import it.polimi.ingsw.am17.Model.GameCard.GameCard;
+
+import java.util.List;
+
+enum Color{
+    RED,
+    BLUE,
+    GREEN,
+    PURPLE,
+    YELLOW,
+    PINK,
+}
+public class Player implements Subject{
+    private String nickname;
+    private int pp;
+    private int food;
+    private Color color;
+    private PlayerRow row;
+    private OfferingCard offeringCard;
+
+    public void addPp(int quantity){}
+    public void addFood(int quantity) {}
+
+
+    public OfferingCard getOfferingCard() {
+        return offeringCard;
+    }
+    public int getPp()
+    {
+        return pp;
+    }
+    public int getFood()
+    {
+        return food;
+    }
+    public Color getColor()
+    {
+        return color;
+    }
+    public void setColor(Color color) {
+        this.color = color;
+    }
+    public void setNickname(String nickname)
+    {
+        this.nickname = nickname;
+    }
+    public String getNickname()
+    {
+        return nickname;
+    }
+
+
+    public void setOfferingCard(OfferingCard offeringCard) {
+        //controllare che l'input sia valido
+    }
+    public void freeOfferingCard(OfferingCard card){}
+    public void playTurn(List<GameCard> cards){}
+
+    @Override
+    public void attach(Observer observer) {
+
+    }
+
+    @Override
+    public void detach(Observer observer) {
+
+    }
+
+    @Override
+    public void notifyObserver() {
+
+    }
+}
