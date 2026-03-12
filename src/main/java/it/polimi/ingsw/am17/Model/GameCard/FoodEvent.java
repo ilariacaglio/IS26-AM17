9b6879024d3d28e187271c6f77d3bb5653d05269
MarcoCR04 <marco.crippa@issgreppi.it>
2026-03-12 16:17:13 +0100

    added FoodEvent

---
+++ src/main/java/it/polimi/ingsw/am17/Model/GameCard/FoodEvent.java
@@ -0,0 +1,9 @@
+package it.polimi.ingsw.am17.Model.GameCard;
+
+public class FoodEvent extends EventCard{
+    private int pointLost;
+
+    public int getPointLost() {
+        return pointLost;
+    }
+}
