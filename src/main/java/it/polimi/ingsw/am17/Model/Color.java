005d938f28b42bcad846a49658d151ca1fe65611
ilariacaglio <ilaria.caglio@issgreppi.it>
2026-03-17 15:24:16 +0100

    prima implementazione metodi player

---
+++ src/main/java/it/polimi/ingsw/am17/Model/Color.java
@@ -0,0 +1,9 @@
+package it.polimi.ingsw.am17.Model;
+
+public enum Color {
+    RED,
+    BLUE,
+    WHITE,
+    BLACK,
+    YELLOW
+}
--- src/main/java/it/polimi/ingsw/am17/Model/Player.java
+++ src/main/java/it/polimi/ingsw/am17/Model/Player.java
@@ -1,63 +1,80 @@
 package it.polimi.ingsw.am17.Model;
 
+import it.polimi.ingsw.am17.Model.GameCard.BuildingCard;
 import it.polimi.ingsw.am17.Model.GameCard.GameCard;
-
 import java.util.List;
 
-enum Color{
-    RED,
-    BLUE,
-    GREEN,
-    PURPLE,
-    YELLOW,
-    PINK,
-}
 public class Player implements Subject{
-    private String nickname;
+    private final String nickname;
     private int pp;
     private int food;
-    private Color color;
-    private PlayerRow row;
+    private final Color color;
+    private final PlayerRow playerRow = new PlayerRow();
     private OfferingCard offeringCard;
 
-    public void addPp(int quantity){}
-    public void addFood(int quantity) {}
+    public Player(String nickname, Color color)
+    {
+        this.nickname = nickname;
+        this.color = color;
+    }
 
+    public void addPp(int quantity){
+        this.pp+=quantity;
+    }
+
+    public void addFood(int quantity) {
+        this.food+=quantity;
+    }
 
     public OfferingCard getOfferingCard() {
         return offeringCard;
     }
+
     public int getPp()
     {
         return pp;
     }
+
     public int getFood()
     {
         return food;
     }
+
     public Color getColor()
     {
         return color;
     }
-    public void setColor(Color color) {
-        this.color = color;
-    }
-    public void setNickname(String nickname)
-    {
-        this.nickname = nickname;
-    }
+
     public String getNickname()
     {
         return nickname;
     }
 
-
     public void setOfferingCard(OfferingCard offeringCard) {
-        //controllare che l'input sia valido
+        //controllo input nel controller
+        this.offeringCard = offeringCard;
     }
-    public void freeOfferingCard(){}
-    public void playTurn(List<GameCard> cards){}
 
+    public void freeOfferingCard() {
+        this.offeringCard = null;
+    }
+
+    //to fix
+    //mi servono dei metodi nel game per modificare le rows
+    public void playTurn(List<GameCard> cards){
+        for(GameCard card : cards){
+            //metodi del game per modificare le rows
+            playerRow.addCard(card);
+        }
+    }
+
+    //to fix
+    public void buyBuilding(BuildingCard card){
+        //manca metodo get nella card
+        addFood(card.getFoodCost()*(-1));
+    }
+
+    //non servono probabilmente
     @Override
     public void attach(Observer observer) {
 
