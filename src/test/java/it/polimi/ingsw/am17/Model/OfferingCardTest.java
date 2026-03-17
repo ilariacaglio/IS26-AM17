c8c252a901f1541aa1d203096836c2a87491b8a5
kerbless <kerbless@proton.me>
2026-03-17 16:03:18 +0100

    Created OfferingCardTest

---
+++ src/test/java/it/polimi/ingsw/am17/Model/OfferingCardTest.java
@@ -0,0 +1,49 @@
+package it.polimi.ingsw.am17.Model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class OfferingCardTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getOrderLetter() {
+    }
+
+    @Test
+    void getFoodBonus() {
+    }
+
+    @Test
+    void getNumCardsLower() {
+    }
+
+    @Test
+    void getMinPlayers() {
+    }
+
+    @Test
+    void getNumCardsUpper() {
+    }
+
+    @Test
+    void attach() {
+    }
+
+    @Test
+    void detach() {
+    }
+
+    @Test
+    void notifyObserver() {
+    }
+}
