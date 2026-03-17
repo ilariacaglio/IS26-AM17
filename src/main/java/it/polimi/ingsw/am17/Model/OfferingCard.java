4c1c561c42750b98f633dd73a78770a7a6a09b66
kerbless <kerbless@proton.me>
2026-03-17 19:45:46 +0100

    made attributes final

--- src/main/java/it/polimi/ingsw/am17/Model/OfferingCard.java
+++ src/main/java/it/polimi/ingsw/am17/Model/OfferingCard.java
@@ -1,11 +1,11 @@
 package it.polimi.ingsw.am17.Model;
 
 public class OfferingCard implements Subject {
-    private int minPlayers;
-    private char orderLetter;
-    private int foodBonus;
-    private int numCardsUpper;
-    private int numCardsLower;
+    private final int minPlayers;
+    private final char orderLetter;
+    private final int foodBonus;
+    private final int numCardsUpper;
+    private final int numCardsLower;
 
     public OfferingCard(int minPlayers, char orderLetter, int foodBonus, int numCardsUpper, int numCardsLower) {
         this.minPlayers = minPlayers;
