7a3213616b4edab52b5f1f1ddf3c1d7ad358d142
ilariacaglio <ilaria.caglio@issgreppi.it>
2026-03-18 15:30:18 +0100

    issue #4 fixed

--- src/main/java/it/polimi/ingsw/am17/Model/Subject.java
+++ src/main/java/it/polimi/ingsw/am17/Model/Subject.java
@@ -1,7 +1,7 @@
 package it.polimi.ingsw.am17.Model;
 
-public interface Subject {
-    void attach(Observer observer);
-    void detach(Observer observer);
-    void notifyObserver();
+public abstract class Subject {
+    abstract void attach(Observer observer);
+    abstract void detach(Observer observer);
+    abstract void notifyObserver();
 }
