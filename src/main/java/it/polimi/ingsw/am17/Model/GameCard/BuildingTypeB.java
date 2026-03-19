1088f0a56b7ec166ce5f6cb7c899e512d7218ee8
kerbless <kerbless@proton.me>
2026-03-19 19:21:31 +0100

    implementing TypeB effect

--- src/main/java/it/polimi/ingsw/am17/Model/GameCard/BuildingTypeB.java
+++ src/main/java/it/polimi/ingsw/am17/Model/GameCard/BuildingTypeB.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.am17.Model.GameCard;
 
+import it.polimi.ingsw.am17.Model.OfferingCard;
 import it.polimi.ingsw.am17.Model.Player;
 
 /**
@@ -14,7 +15,8 @@ public class BuildingTypeB extends BuildingCard {
 
     @Override
     public void resolveEffect(Player player) {
-        // TODO: idea è fare una pseudo offeringCard.
-        player.setOfferingCard();
+        OfferingCard pseudoOfferingCard = new OfferingCard(2, 'Z', 0, 1, 0);
+        // TODO: make sure the player has not his offering card anymore and that this is called before the game stops considering new turns.
+        player.setOfferingCard(pseudoOfferingCard);
     }
 }
