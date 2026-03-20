544aa78486b11b3536f426cd1d08aceec30920a3
kerbless <kerbless@proton.me>
2026-03-20 15:16:08 +0100

    concept implementation of TypeC building

--- src/main/java/it/polimi/ingsw/am17/Model/GameCard/BuildingTypeC.java
+++ src/main/java/it/polimi/ingsw/am17/Model/GameCard/BuildingTypeC.java
@@ -1,30 +1,32 @@
 package it.polimi.ingsw.am17.Model.GameCard;
 
 import it.polimi.ingsw.am17.Model.CardRow;
-import it.polimi.ingsw.am17.Model.OfferingCard;
 import it.polimi.ingsw.am17.Model.Player;
 
 /**
- * Gives extra points for each character of given type.
+ * Gives extra pointsFromEachCharacter for each character of given type.
  * Final effect
  * MULTIPLE
  */
 public class BuildingTypeC extends BuildingCard {
-//    private string characterType;
-    private int points; // Extra points for each characterType
+    private int pointsFromEachCharacter; // Extra pointsFromEachCharacter for each characterType
 
-    public BuildingTypeC(int foodCost, int bonusPoints, CharacterCard characterType) {
+    public BuildingTypeC(int foodCost, int bonusPoints, CharacterCard characterType, int pointsFromEachCharacter) {
         super(3, foodCost, bonusPoints);
         this.characterType = characterType;
-        this.points = bonusPoints;
+        this.pointsFromEachCharacter = pointsFromEachCharacter;
     }
 
     @Override
-    public void resolveEffect(CardRow cardRow) {
-        for (GameCard card : cardRow.getCards()) {
-            if (card instanceof Hunter) {
-
+    public void resolveEffect(Player player) {
+        CardRow playerRow = player.getCardRow();
+        int count = 0;
+        for (GameCard card : playerRow.getCards()) {
+            if card is of class characterType {
+                count++;
             }
         }
+
+        player.addPp(count * pointsFromEachCharacter);
     }
 }
