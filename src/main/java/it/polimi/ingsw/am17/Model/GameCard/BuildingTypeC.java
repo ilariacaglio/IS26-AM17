3dce930b55cb6fe9f77a9420e5bd4c5741d75f78
kerbless <kerbless@proton.me>
2026-03-20 15:03:28 +0100

    developing TypeC building

--- src/main/java/it/polimi/ingsw/am17/Model/GameCard/BuildingTypeC.java
+++ src/main/java/it/polimi/ingsw/am17/Model/GameCard/BuildingTypeC.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.am17.Model.GameCard;
 
+import it.polimi.ingsw.am17.Model.CardRow;
 import it.polimi.ingsw.am17.Model.OfferingCard;
 import it.polimi.ingsw.am17.Model.Player;
 
@@ -9,14 +10,21 @@ import it.polimi.ingsw.am17.Model.Player;
  * MULTIPLE
  */
 public class BuildingTypeC extends BuildingCard {
-    public BuildingTypeC() {
-        super(3, 9, 3);
+//    private string characterType;
+    private int points; // Extra points for each characterType
+
+    public BuildingTypeC(int foodCost, int bonusPoints, CharacterCard characterType) {
+        super(3, foodCost, bonusPoints);
+        this.characterType = characterType;
+        this.points = bonusPoints;
     }
 
     @Override
-    public void resolveEffect(Player player) {
-        OfferingCard pseudoOfferingCard = new OfferingCard(2, 'Z', 0, 1, 0);
-        // TODO: make sure the player has not his offering card anymore and that this is called before the game stops considering new turns.
-        player.setOfferingCard(pseudoOfferingCard);
+    public void resolveEffect(CardRow cardRow) {
+        for (GameCard card : cardRow.getCards()) {
+            if (card instanceof Hunter) {
+
+            }
+        }
     }
 }
