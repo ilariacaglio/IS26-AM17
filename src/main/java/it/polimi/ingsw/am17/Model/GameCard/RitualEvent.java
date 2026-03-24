4ba418492adc83323aa84221ac2a4b5d63327286
saracellegato <sara.cellegato@mail.polimi.it>
2026-03-24 14:37:37 +0100

    added computeScore method for class RitualEvent

--- src/main/java/it/polimi/ingsw/am17/Model/GameCard/RitualEvent.java
+++ src/main/java/it/polimi/ingsw/am17/Model/GameCard/RitualEvent.java
@@ -22,5 +22,40 @@ public class RitualEvent extends EventCard {
         this.pointMin = pointMin;
     }
     @Override
-    public void computeScore(List<Player> list){}
+    public void computeScore(List<Player> list){
+        int[] stars = new int[list.size()];
+
+        for(int i = 0; i < list.size(); i++){
+            stars[i] = list.get(i).getPlayerCards().stream()
+                    .filter(c -> c instanceof Shaman)
+                    .mapToInt(c -> ((Shaman)c).getStars())
+                    .sum();
+        }
+
+        int max = stars[0];
+        int min = stars[0];
+
+        for(int i = 1; i < stars.length; i++){
+            if(stars[i] > max){ max = stars[i]; }
+            if(stars[i] < min){ min = stars[i]; }
+        }
+
+        boolean allEqual = (max == min);
+
+        for(int i = 0; i < list.size(); i++){
+            if(allEqual){
+                list.get(i).addPp(pointMax);
+                list.get(i).addPp(pointMin*(-1));
+            }
+            else{
+                if(stars[i] == max){
+                    list.get(i).addPp(pointMax);
+                }
+                if(stars[i] == min){
+                    list.get(i).addPp(pointMin*(-1));
+                }
+            }
+        }
+
+    }
 }
