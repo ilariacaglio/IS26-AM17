2bf3a4b65cb32305ddcbb268674f96b8019607c5
kerbless <kerbless@proton.me>
2026-03-26 16:50:28 +0100

    added attribute characterType for 3M building

--- src/main/java/it/polimi/ingsw/am17/Model/GameCard/BuildingType3M.java
+++ src/main/java/it/polimi/ingsw/am17/Model/GameCard/BuildingType3M.java
@@ -10,6 +10,7 @@ import it.polimi.ingsw.am17.Model.Player;
  */
 public class BuildingType3M extends BuildingCard {
     private int pointsFromEachCharacter; // Extra pointsFromEachCharacter for each characterType
+    private CharacterCard characterType;
 
     public BuildingType3M(int foodCost, int bonusPoints, CharacterCard characterType, int pointsFromEachCharacter) {
         super(3, foodCost, bonusPoints);
