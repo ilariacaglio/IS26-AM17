dc40732d38cc1b4dd17c6b98195cae679bb59af2
kerbless <kerbless@proton.me>
2026-03-26 16:04:30 +0100

    readded missing 13M building

---
+++ src/main/java/it/polimi/ingsw/am17/Model/GameCard/BuildingType13M.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.am17.Model.GameCard;
+
+import it.polimi.ingsw.am17.Model.Player;
+
+/**
+ * Save food for each character type when FoodEvent
+ * Event effect
+ * MULTIPLE
+ */
+public class BuildingType13M extends BuildingCard {
+    public BuildingType13M() {
+        super(2, 7, 4);
+    }
