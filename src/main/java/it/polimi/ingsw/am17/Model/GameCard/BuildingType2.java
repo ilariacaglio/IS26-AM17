12ddf0212d079057b77dd11093f30d9c0dc7e822
kerbless <kerbless@proton.me>
2026-03-26 16:04:09 +0100

    sample implementation of type 2 building

--- src/main/java/it/polimi/ingsw/am17/Model/GameCard/BuildingType2.java
+++ src/main/java/it/polimi/ingsw/am17/Model/GameCard/BuildingType2.java
@@ -14,7 +14,7 @@ public class BuildingType2 extends BuildingCard {
     }
 
     @Override
-    public void resolveEffect(Player player) {
+    public void playExtraCard(Player player) {
         OfferingCard pseudoOfferingCard = new OfferingCard(2, 'Z', 0, 1, 0);
         // TODO: make sure the player has not his offering card anymore and that this is called before the game stops considering new turns.
         player.setOfferingCard(pseudoOfferingCard);
