fe891674c02b77e8926cd626db199e7aa60db3c5
kerbless <kerbless@proton.me>
2026-03-26 16:05:11 +0100

    readded missing type 8 building

---
+++ src/main/java/it/polimi/ingsw/am17/Model/GameCard/BuildingType8.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.am17.Model.GameCard;
+
+/**
+ * Get double the points from RitualEvent if "won". Get the points even if tied.
+ * Event effect
+ * SINGLETON
+ */
+public class BuildingType8 extends BuildingCard {
+    public BuildingType8() {
+        super(2, 6, 4);
+    }
+
+}
