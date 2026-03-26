e5be932b1d871bc2fac3257b076a09cc790b4e9e
kerbless <kerbless@proton.me>
2026-03-26 16:49:32 +0100

    switch to return for pp management type3m building

--- src/main/java/it/polimi/ingsw/am17/Model/GameCard/BuildingType3M.java
+++ src/main/java/it/polimi/ingsw/am17/Model/GameCard/BuildingType3M.java
@@ -28,6 +28,6 @@ public class BuildingType3M extends BuildingCard {
 //            }
         }
 
-        player.addPp(count * pointsFromEachCharacter);
+        return count * pointsFromEachCharacter;
     }
 }
