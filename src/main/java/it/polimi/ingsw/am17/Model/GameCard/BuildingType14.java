5c82b63abfeb4e0f06159f41342b232e444f14e4
kerbless <kerbless@proton.me>
2026-03-27 18:51:34 +0100

    fix comment effect type BuildingType14

--- src/main/java/it/polimi/ingsw/am17/Model/GameCard/BuildingType14.java
+++ src/main/java/it/polimi/ingsw/am17/Model/GameCard/BuildingType14.java
@@ -2,7 +2,7 @@ package it.polimi.ingsw.am17.Model.GameCard;
 
 /**
  * Get 5 food each time you complete a set of 6 cards.
- * Event effect
+ * Card effect
  * SINGLETON
  */
 public class BuildingType14 extends BuildingCard {
