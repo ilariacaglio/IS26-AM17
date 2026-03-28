3f79acca7e0ecbf61d4a20e5dd552b8ac64ef3f0
kerbless <kerbless@proton.me>
2026-03-28 10:52:38 +0100

    BuildingType14 implementation (check)

--- src/main/java/it/polimi/ingsw/am17/Model/GameCard/BuildingType14.java
+++ src/main/java/it/polimi/ingsw/am17/Model/GameCard/BuildingType14.java
@@ -1,5 +1,10 @@
 package it.polimi.ingsw.am17.Model.GameCard;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 /**
  * Get 5 food each time you complete a set of 6 cards.
  * Card effect
@@ -9,5 +14,27 @@ public class BuildingType14 extends BuildingCard {
     public BuildingType14() {
         super(2, 6, 4);
     }
-    // TODO
+
+    @Override
+    public int FoodBonus(List<CharacterCard> characterCards, CharacterCard newCard) {
+
+        // TODO: check if working
+        // Count how many cards of each character type with some stream magic
+        Map<CardType, Long> typeCount = characterCards.stream()
+                .collect(Collectors.groupingBy(
+                        CharacterCard::getCardType,
+                        Collectors.counting()
+                ));
+
+        // If you're missing any characterType different than the new one, you don't have a new set
+        for  (Map.Entry<CardType, Long> entry : typeCount.entrySet()) {
+            if (entry.getKey() != newCard.getCardType())
+                if (entry.getValue() == 0) {
+                    return 0;
+                }
+        }
+
+        // else you do and get the points
+        return 5;
+    }
 }
