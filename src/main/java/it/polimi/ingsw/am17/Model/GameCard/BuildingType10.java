f10b80e78a64bc3f7c20d5e548d64fd5d24606bb
kerbless <kerbless@proton.me>
2026-03-28 10:39:20 +0100

    BuildingType10 implementation

--- src/main/java/it/polimi/ingsw/am17/Model/GameCard/BuildingType10.java
+++ src/main/java/it/polimi/ingsw/am17/Model/GameCard/BuildingType10.java
@@ -1,5 +1,8 @@
 package it.polimi.ingsw.am17.Model.GameCard;
 
+import java.util.List;
+import java.util.Objects;
+
 /**
  * Get 3 food each time you pick an inventor you already have.
  * Card effect
@@ -7,7 +10,31 @@ package it.polimi.ingsw.am17.Model.GameCard;
  */
 public class BuildingType10 extends BuildingCard {
     public BuildingType10() {
-        super(2, 6, 4);
+        super(1, 3, 4);
     }
-    // TODO
+
+    @Override
+    public int FoodBonus(List<CharacterCard> characterCards, CharacterCard newCard) {
+
+        // if new card is inventor
+        if (newCard.getCardType() == CardType.INVENTOR) {
+            Inventor newInventor = (Inventor) newCard;
+
+            // get inventors had already with stream magic
+            List<Inventor> inventors = characterCards.stream()
+                    .filter(card -> card.getCardType() == CardType.INVENTOR)
+                    .map(card -> (Inventor) card)
+                    .toList();
+
+            for (Inventor inventor : inventors) {
+                if (Objects.equals(inventor.getIcon(), newInventor.getIcon())) {
+                    return 3;
+                }
+            }
+        }
+
+        // Else return 0
+        return 0;
+    }
+
 }
