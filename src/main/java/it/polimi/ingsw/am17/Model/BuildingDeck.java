bec2745fc0888b9a064bafc691901ad498195518
ilariacaglio <ilaria.caglio@issgreppi.it>
2026-03-29 15:46:40 +0200

    fix data types in BuildingDeck.java and Deck.java

--- src/main/java/it/polimi/ingsw/am17/Model/BuildingDeck.java
+++ src/main/java/it/polimi/ingsw/am17/Model/BuildingDeck.java
@@ -6,14 +6,14 @@ import it.polimi.ingsw.am17.Model.GameCard.GameCard;
 import java.util.List;
 
 public class BuildingDeck {
-    private List<GameCard> buildingCards;
+    private List<BuildingCard> buildingCards;
 
     public BuildingDeck(int numPlayer)
     {
         buildingCards = null; //creare metodo per inizializzare il mazzo
     }
 
-    public List<GameCard> drawAll(){
+    public List<BuildingCard> drawAll(){
         return buildingCards;
     }
 }
