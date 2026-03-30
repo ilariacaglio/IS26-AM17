b67151088c98ea06e9684aa2274e724a5b230f27
ilariacaglio <ilaria.caglio@issgreppi.it>
2026-03-30 19:32:26 +0200

    create building deck feature

--- src/main/java/it/polimi/ingsw/am17/Model/BuildingDeck.java
+++ src/main/java/it/polimi/ingsw/am17/Model/BuildingDeck.java
@@ -1,18 +1,63 @@
 package it.polimi.ingsw.am17.Model;
 
-import it.polimi.ingsw.am17.Model.GameCard.BuildingCard;
-import it.polimi.ingsw.am17.Model.GameCard.GameCard;
+import it.polimi.ingsw.am17.Model.GameCard.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class BuildingDeck {
-    private List<BuildingCard> buildingCards;
+import static it.polimi.ingsw.am17.Utility.BuildingCardParser.buildingCard13MParser;
+import static it.polimi.ingsw.am17.Utility.BuildingCardParser.buildingCard3MParser;
 
-    public BuildingDeck(int numPlayer)
+public class BuildingDeck {
+    private final List<BuildingCard> buildingCards;
+
+    public BuildingDeck(int numPlayer, int era)
     {
-        buildingCards = null; //creare metodo per inizializzare il mazzo
+        buildingCards = new ArrayList<>();
+        switch(era){
+            case 1:
+                buildEra1Deck();
+                break;
+            case 2:
+                buildEra2Deck();
+                break;
+            case 3:
+                buildEra3Deck();
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid era value");
+        }
     }
 
+    private void buildEra1Deck(){
+        //singleton building cards
+        buildingCards.add(new BuildingType10());
+        buildingCards.add(new BuildingType11());
+        buildingCards.add(new BuildingType12());
+        buildingCards.add(new BuildingType14());
+        //parse from json and add cards 13M
+        buildingCards.addAll(buildingCard13MParser(1));
+    }
+
+    private void buildEra2Deck(){
+        //singleton building cards
+        buildingCards.add(new BuildingType4());
+        buildingCards.add(new BuildingType5());
+        buildingCards.add(new BuildingType6());
+        buildingCards.add(new BuildingType7());
+        buildingCards.add(new BuildingType8());
+        buildingCards.add(new BuildingType9());
+        //parse from json and add cards 13M
+        buildingCards.addAll(buildingCard13MParser(2));
+    }
+
+    private  void buildEra3Deck(){
+        //singleton building cards
+        buildingCards.add(new BuildingType1());
+        buildingCards.add(new BuildingType2());
+        //parse from json and add cards 3M
+        buildingCards.addAll(buildingCard3MParser());
+    }
     public List<BuildingCard> drawAll(){
         return buildingCards;
     }
