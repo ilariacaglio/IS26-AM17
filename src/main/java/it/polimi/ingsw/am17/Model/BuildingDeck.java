05a097acbc5589336537101dda6198bd398ff8ac
ilariacaglio <ilaria.caglio@issgreppi.it>
2026-03-30 20:21:05 +0200

    added size control

--- src/main/java/it/polimi/ingsw/am17/Model/BuildingDeck.java
+++ src/main/java/it/polimi/ingsw/am17/Model/BuildingDeck.java
@@ -3,6 +3,7 @@ package it.polimi.ingsw.am17.Model;
 import it.polimi.ingsw.am17.Model.GameCard.*;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static it.polimi.ingsw.am17.Utility.BuildingCardParser.buildingCard13MParser;
@@ -11,52 +12,85 @@ import static it.polimi.ingsw.am17.Utility.BuildingCardParser.buildingCard3MPars
 public class BuildingDeck {
     private final List<BuildingCard> buildingCards;
 
-    public BuildingDeck(int numPlayer, int era)
+    public BuildingDeck(int numPlayers, int era)
     {
         buildingCards = new ArrayList<>();
         switch(era){
             case 1:
-                buildEra1Deck();
+                buildEra1Deck(numPlayers);
                 break;
             case 2:
-                buildEra2Deck();
+                buildEra2Deck(numPlayers);
                 break;
             case 3:
-                buildEra3Deck();
+                buildEra3Deck(numPlayers);
                 break;
             default:
                 throw new IllegalArgumentException("Invalid era value");
         }
     }
 
-    private void buildEra1Deck(){
+    private void buildEra1Deck(int numPlayers){
+        List<BuildingCard> temp = new ArrayList<>();
         //singleton building cards
-        buildingCards.add(new BuildingType10());
-        buildingCards.add(new BuildingType11());
-        buildingCards.add(new BuildingType12());
-        buildingCards.add(new BuildingType14());
+        temp.add(new BuildingType10());
+        temp.add(new BuildingType11());
+        temp.add(new BuildingType12());
+        temp.add(new BuildingType14());
         //parse from json and add cards 13M
-        buildingCards.addAll(buildingCard13MParser(1));
+        temp.addAll(buildingCard13MParser(1));
+        //shuffle collection
+        Collections.shuffle(temp);
+        //add the right number of cards based on numPlayer
+        if(numPlayers == 2) {
+            buildingCards.add(temp.getFirst());
+        }
+        else {
+            buildingCards.addAll(temp.subList(0,2));
+        }
     }
 
-    private void buildEra2Deck(){
+    private void buildEra2Deck(int numPlayers){
+        List<BuildingCard> temp = new ArrayList<>();
         //singleton building cards
-        buildingCards.add(new BuildingType4());
-        buildingCards.add(new BuildingType5());
-        buildingCards.add(new BuildingType6());
-        buildingCards.add(new BuildingType7());
-        buildingCards.add(new BuildingType8());
-        buildingCards.add(new BuildingType9());
+        temp.add(new BuildingType4());
+        temp.add(new BuildingType5());
+        temp.add(new BuildingType6());
+        temp.add(new BuildingType7());
+        temp.add(new BuildingType8());
+        temp.add(new BuildingType9());
         //parse from json and add cards 13M
-        buildingCards.addAll(buildingCard13MParser(2));
+        temp.addAll(buildingCard13MParser(2));
+        //shuffle collection
+        Collections.shuffle(temp);
+        //add the right number of cards based on numPlayer
+        if(numPlayers <= 3) {
+            buildingCards.addAll(temp.subList(0,2));
+        }
+        else {
+            buildingCards.addAll(temp.subList(0,3));
+        }
     }
 
-    private  void buildEra3Deck(){
+    private void buildEra3Deck(int numPlayers){
+        List<BuildingCard> temp = new ArrayList<>();
         //singleton building cards
-        buildingCards.add(new BuildingType1());
-        buildingCards.add(new BuildingType2());
+        temp.add(new BuildingType1());
+        temp.add(new BuildingType2());
         //parse from json and add cards 3M
-        buildingCards.addAll(buildingCard3MParser());
+        temp.addAll(buildingCard3MParser());
+        //shuffle collection
+        Collections.shuffle(temp);
+        //add the right number of cards based on numPlayer
+        if(numPlayers == 2) {
+            buildingCards.addAll(temp.subList(0,3));
+        }
+        else if (numPlayers == 3 || numPlayers == 4) {
+            buildingCards.addAll(temp.subList(0,4));
+        }
+        else{
+            buildingCards.addAll(temp.subList(0,5));
+        }
     }
     public List<BuildingCard> drawAll(){
         return buildingCards;
