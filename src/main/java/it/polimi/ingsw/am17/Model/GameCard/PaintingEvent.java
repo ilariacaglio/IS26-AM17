6c736b5acb0ee6e5397525a8e4de584e45b4fdca
ilariacaglio <ilaria.caglio@issgreppi.it>
2026-03-31 12:05:23 +0200

    changed constructor parameters in PaintingEvent.java

--- src/main/java/it/polimi/ingsw/am17/Model/GameCard/PaintingEvent.java
+++ src/main/java/it/polimi/ingsw/am17/Model/GameCard/PaintingEvent.java
@@ -5,10 +5,10 @@ import it.polimi.ingsw.am17.Model.Player;
 import java.util.List;
 
 public class PaintingEvent extends EventCard {
-    private int pointsLow;
-    private int pointsMax;
-    private int numLow;
-    private int numMax;
+    private final int pointsLow;
+    private final int pointsMax;
+    private final int numLow;
+    private final int numMax;
 
     public int getNumLow() {
         return numLow;
@@ -26,8 +26,8 @@ public class PaintingEvent extends EventCard {
         return pointsMax;
     }
 
-    public PaintingEvent(int pointsLow, int pointsMax, int numLow, int numMax, boolean Final, int era, CardType cardType) {
-        super(Final, era, cardType);
+    public PaintingEvent(int pointsLow, int pointsMax, int numLow, int numMax, boolean Final, int era) {
+        super(Final, era, CardType.PAINTING_EVENT);
         this.pointsLow = pointsLow;
         this.pointsMax = pointsMax;
         this.numLow = numLow;
