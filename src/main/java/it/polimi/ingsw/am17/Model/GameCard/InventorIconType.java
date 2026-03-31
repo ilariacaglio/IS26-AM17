a20d8ea6196fea32ee7f6bf28962fbad0594c3ed
ilariacaglio <ilaria.caglio@issgreppi.it>
2026-03-31 11:54:41 +0200

    Added Inventor.json, fix constructor and Enum for icon

---
+++ src/main/java/it/polimi/ingsw/am17/Model/GameCard/InventorIconType.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.am17.Model.GameCard;
+
+public enum InventorIconType {
+    FLUTE,
+    MORTAR,
+    ROPE,
+    LEATHER,
+    BREAD,
+    CANOE,
+    HOOK,
+    IDOL,
+    NECKLACE,
+    FLINT
+}
