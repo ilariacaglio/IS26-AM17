bb8afb63225405ef4251a8788929adfd224d2e30
kerbless <kerbless@proton.me>
2026-03-31 12:52:33 +0200

    better and new tests for player

--- src/test/java/it/polimi/ingsw/am17/Model/PlayerTest.java
+++ src/test/java/it/polimi/ingsw/am17/Model/PlayerTest.java
@@ -1,8 +1,13 @@
 package it.polimi.ingsw.am17.Model;
 
+import it.polimi.ingsw.am17.Model.GameCard.CardType;
+import it.polimi.ingsw.am17.Model.GameCard.TribesCard;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -41,21 +46,29 @@ class PlayerTest {
     }
 
     @Test
-    void addFoodShouldAddFood() {
+    void addFoodShouldChangeFood() {
         int foodChange = 5;
         player.addFood(foodChange);
         assertEquals(foodChange, player.getFood());
+        player.addFood(-foodChange);
+        assertEquals(0, player.getFood());
     }
 
     @Test
-    void addFoodShouldRemoveFood() {
-        int foodChange = -5;
-        player.addFood(foodChange);
-        assertEquals(foodChange, player.getFood());
+    void addFoodShouldNotGoUnderZero() {
+        assertThrows(IllegalStateException.class, () -> player.addFood(-5));
     }
 
     @Test
-    void setOfferingCard() {
+    void playTurnWithOfferingCard() {
+        OfferingCard offeringCard = new OfferingCard(2,'A', 1 ,1, 1);
+        TribesCard tribeCard1 = new TribesCard(1, CardType.INVENTOR);
+        TribesCard tribeCard2 = new TribesCard(1, CardType.PAINTING_EVENT);
+        List<TribesCard>  tribeCards = List.of(tribeCard1, tribeCard2);
+        Game game = new Game(3);
+        game.getUpperRow().addCard(tribeCard1);
+        game.getUpperRow().addCard(tribeCard2);
+        player.playTurn(tribeCards, null, game);
     }
 
     @Test
