864214ea168e0e3329a95eda29605a5f15c642b0
ilariacaglio <ilaria.caglio@issgreppi.it>
2026-03-31 11:24:29 +0200

    Added Shaman.json and fix constructor

--- src/main/java/it/polimi/ingsw/am17/Model/GameCard/Shaman.java
+++ src/main/java/it/polimi/ingsw/am17/Model/GameCard/Shaman.java
@@ -2,14 +2,14 @@ package it.polimi.ingsw.am17.Model.GameCard;
 
 
 public class Shaman extends CharacterCard {
-    private int stars;
+    private final int stars;
 
     public int getStars() {
         return stars;
     }
 
-    public Shaman(int era, int minPlayer, int stars,CardType cardType){
-        super(era, minPlayer, cardType);
+    public Shaman(int era, int minPlayer, int stars){
+        super(era, minPlayer, CardType.SHAMAN);
         this.stars = stars;
     }
 }
