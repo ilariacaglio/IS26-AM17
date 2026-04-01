4b847714057f24eac90bc83689cdf1404340b585
ilariacaglio <ilaria.caglio@issgreppi.it>
2026-04-01 14:38:16 +0200

    Json annotations in FoodEvent.java

--- src/main/java/it/polimi/ingsw/am17/Model/GameCard/FoodEvent.java
+++ src/main/java/it/polimi/ingsw/am17/Model/GameCard/FoodEvent.java
@@ -1,17 +1,23 @@
 package it.polimi.ingsw.am17.Model.GameCard;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import it.polimi.ingsw.am17.Model.Player;
 
 import java.util.List;
 
 public class FoodEvent extends EventCard{
-    private int pointLost;
+    private final int pointLost;
 
     public int getPointLost() {
         return pointLost;
     }
 
-    public FoodEvent(int pointLost, boolean Final, int era){
+    @JsonCreator
+    public FoodEvent(
+           @JsonProperty("pointLost") int pointLost,
+           @JsonProperty("Final") boolean Final,
+           @JsonProperty("era") int era){
         super(Final, era, CardType.FOOD_EVENT);
         this.pointLost = pointLost;
     }
