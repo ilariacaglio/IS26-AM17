3756398fe569a482aecbbbb57930a1e941d5f877
MarcoCR <crippa.marco04@gmail.com>
2026-04-02 19:11:23 +0200

    added BuildingType5Test.setup

--- src/test/java/it/polimi/ingsw/am17/Model/GameCard/BuildingType5Test.java
+++ src/test/java/it/polimi/ingsw/am17/Model/GameCard/BuildingType5Test.java
@@ -1,7 +1,20 @@
 package it.polimi.ingsw.am17.Model.GameCard;
 
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BuildingType5Test {
-
+    BuildingType5 building;
+    Random r = new Random();
+    @BeforeEach
+    /**
+     * Generate a BuildingType5 with random value and stores the value of pointsFromEachCharacter
+     */
+    void setup()
+    {
+        building = new BuildingType5();
+    }
 }
