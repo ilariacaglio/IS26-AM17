4ebfb9e5ee366920a9912e86966faa87dd9c6c40
MarcoCR <crippa.marco04@gmail.com>
2026-04-02 19:54:36 +0200

    added BuildingType7Test.FoodBonusAndPointsBonusTest

--- src/test/java/it/polimi/ingsw/am17/Model/GameCard/BuildingType7Test.java
+++ src/test/java/it/polimi/ingsw/am17/Model/GameCard/BuildingType7Test.java
@@ -1,12 +1,14 @@
 package it.polimi.ingsw.am17.Model.GameCard;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class BuildingType7Test {
+class BuildingType7Test extends BuildingCardTest{
     BuildingType7 building;
     Random r = new Random();
     @BeforeEach
@@ -16,5 +18,30 @@ class BuildingType7Test {
     void setup()
     {
         building = new BuildingType7();
+    }
+
+    @Test
+    /**
+     * Run test of FoodBonus with different list of card generated randomly
+     * and check the return value is correct
+     */
+    void FoodBonusAndPointsBonusTest()
+    {
+        for (int i = 0; i < 5; i++) {
+            int numHunter = r.nextInt(5);
+            int numBuilder = r.nextInt(5);
+            int numArtist = r.nextInt(5);
+            int numInv = r.nextInt(5);
+            int numSham = r.nextInt(5);
+            int numBind = r.nextInt(5);
+            List<CharacterCard> cc = createCharacterCardList(numHunter, numArtist, numBuilder, numInv, numSham, numBind);
+            int fp = building.PointsBonus(cc);
+            int fb = building.FoodBonus(cc);
+
+            assertEquals(numHunter, fp);
+            assertEquals(numHunter, fb);
+        }
+
+
     }
 }
