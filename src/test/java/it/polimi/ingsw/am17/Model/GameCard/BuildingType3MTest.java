1805df1cf90591a1e5213ee07184edecad464d6a
MarcoCR <crippa.marco04@gmail.com>
2026-04-02 18:36:32 +0200

    added BuildingType3MTest.setup

--- src/test/java/it/polimi/ingsw/am17/Model/GameCard/BuildingType3MTest.java
+++ src/test/java/it/polimi/ingsw/am17/Model/GameCard/BuildingType3MTest.java
@@ -1,7 +1,71 @@
 package it.polimi.ingsw.am17.Model.GameCard;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BuildingType3MTest {
+    BuildingType3M building;
+    Random r = new Random();
+    int pointsFromEachCharacter;
+    @BeforeEach
+    /**
+     * Generate a BuildingType3M with random value and stores the value of pointsFromEachCharacter
+     */
+    void setup()
+    {
+        int food = r.nextInt(5);
+        int bonusPoint = r.nextInt(3);
+        pointsFromEachCharacter = r.nextInt(5);
+        building = new BuildingType3M(food, bonusPoint, CardType.HUNTER,pointsFromEachCharacter);
+    }
+
+    @Test
+    /**
+     * Run test of FinalPoints with different list of card generated randomly
+     * and check the return value is correct
+     */
+    void testFinalPoint()
+    {
+        for (int i = 0; i < 5; i++) {
+            int numHunter = r.nextInt(5);
+            int numBuilder = r.nextInt(5);
+            int numArtist = r.nextInt(5);
+            List<CharacterCard> cc = createCharacterCard(numHunter, numArtist, numBuilder);
+
+            int fp = building.FinalPoints(cc);
+
+            assertEquals(numHunter*pointsFromEachCharacter, fp);
+        }
+    }
+
+    /**
+     * Generate and return a list of CharacterCard with number of cards as specified
+     * @param hunter number of hunter you want to have in the list
+     * @param art number of artist you want to have in the list
+     * @param build number of builder you want to have in the list
+     * @return List<CharacterCard>
+     */
+    private List<CharacterCard> createCharacterCard(int hunter, int art, int build)
+    {
+        List<CharacterCard> cc = new ArrayList<>();
+
+        for (int i = 0; i < hunter; i++) {
+            cc.add(new CharacterCard(1, 1, CardType.HUNTER));
+        }
+        for (int i = 0; i < art; i++) {
+            cc.add(new CharacterCard(1, 1, CardType.ARTIST));
+        }
+        for (int i = 0; i < build; i++) {
+            cc.add(new CharacterCard(1, 1, CardType.BUILDER));
+        }
+
+        return cc;
+    }
 
 }
