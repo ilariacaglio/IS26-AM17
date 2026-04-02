01f1b76aef1eeae902d6b4c422ced8b5befbb08e
MarcoCR <crippa.marco04@gmail.com>
2026-04-02 19:34:38 +0200

    added BuildingType7Test.setup

--- src/test/java/it/polimi/ingsw/am17/Model/GameCard/BuildingType7Test.java
+++ src/test/java/it/polimi/ingsw/am17/Model/GameCard/BuildingType7Test.java
@@ -1,7 +1,20 @@
 package it.polimi.ingsw.am17.Model.GameCard;
 
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BuildingType7Test {
-
+    BuildingType7 building;
+    Random r = new Random();
+    @BeforeEach
+    /**
+     * Generate a BuildingType7
+     */
+    void setup()
+    {
+        building = new BuildingType7();
+    }
 }
