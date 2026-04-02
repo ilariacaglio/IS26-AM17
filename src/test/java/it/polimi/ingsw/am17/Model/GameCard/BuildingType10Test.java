af54962e4474882c6f9de2dca8ff47ea8f6e3804
MarcoCR <crippa.marco04@gmail.com>
2026-04-02 22:07:16 +0200

    added BuildingType10Test.setup

--- src/test/java/it/polimi/ingsw/am17/Model/GameCard/BuildingType10Test.java
+++ src/test/java/it/polimi/ingsw/am17/Model/GameCard/BuildingType10Test.java
@@ -1,7 +1,20 @@
 package it.polimi.ingsw.am17.Model.GameCard;
 
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BuildingType10Test {
-
+    BuildingType10 building;
+    Random r = new Random();
+    @BeforeEach
+    /**
+     * Generate a BuildingType10
+     */
+    void setup()
+    {
+        building = new BuildingType10();
+    }
 }
