9fde9f57fb66753ecdb2a75dfdd0a1ab564b83e7
MarcoCR <crippa.marco04@gmail.com>
2026-04-02 18:43:14 +0200

    added BuildingType4Test.createCharacterCardList

--- src/test/java/it/polimi/ingsw/am17/Model/GameCard/BuildingType4Test.java
+++ src/test/java/it/polimi/ingsw/am17/Model/GameCard/BuildingType4Test.java
@@ -2,6 +2,8 @@ package it.polimi.ingsw.am17.Model.GameCard;
 
 import org.junit.jupiter.api.BeforeEach;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,5 +18,41 @@ class BuildingType4Test {
     void setup()
     {
         building = new BuildingType4();
+    }
+
+    /**
+     * Generate and return a list of CharacterCard with number of cards as specified
+     * @param hunter number of hunter you want to have in the list
+     * @param art number of artist you want to have in the list
+     * @param build number of builder you want to have in the list
+     * @param inv number of inventor you want to have in the list
+     * @param sham number of shaman you want to have in the list
+     * @param bind number of binder you want to have in the list
+     * @return List<CharacterCard>
+     */
+    private List<CharacterCard> createCharacterCardList(int hunter, int art, int build, int inv, int sham, int bind)
+    {
+        List<CharacterCard> cc = new ArrayList<>();
+
+        for (int i = 0; i < hunter; i++) {
+            cc.add(new CharacterCard(1, 1, CardType.HUNTER));
+        }
+        for (int i = 0; i < art; i++) {
+            cc.add(new CharacterCard(1, 1, CardType.ARTIST));
+        }
+        for (int i = 0; i < build; i++) {
+            cc.add(new CharacterCard(1, 1, CardType.BUILDER));
+        }
+        for (int i = 0; i < inv; i++) {
+            cc.add(new CharacterCard(1, 1, CardType.INVENTOR));
+        }
+        for (int i = 0; i < build; i++) {
+            cc.add(new CharacterCard(1, 1, CardType.SHAMAN));
+        }
+        for (int i = 0; i < build; i++) {
+            cc.add(new CharacterCard(1, 1, CardType.BINDER));
+        }
+
+        return cc;
     }
 }
