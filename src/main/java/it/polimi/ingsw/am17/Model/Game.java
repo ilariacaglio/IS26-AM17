99cc4cac7277f412dfffe0ee74f0715c7e1ff885
ilariacaglio <ilaria.caglio@issgreppi.it>
2026-04-09 14:10:25 +0200

    added check for offering card in selectOfferingCard

--- src/main/java/it/polimi/ingsw/am17/Model/Game.java
+++ src/main/java/it/polimi/ingsw/am17/Model/Game.java
@@ -296,6 +296,11 @@ public class Game extends Subject {
      */
     public void selectOfferingCard(Player player, OfferingCard offeringCard)
     {
+        //check if offeringCard is valid
+        if(!offeringCards.contains(offeringCard) && !offeringCard.equals(building2OfferingCard)) {
+            throw new IllegalStateException("Illegal card selection. (Card not in any offering)");
+        }
+
         //check if is player turn
         if(!player.equals(orderedPlayer.pop())) {
             throw new IllegalStateException("It is not the player's turn.");
