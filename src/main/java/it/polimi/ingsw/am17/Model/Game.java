628b0834ed1d45bf202fe65b5739fcb53ebc6062
kerbless <kerbless@proton.me>
2026-04-10 14:43:48 +0200

    renamed playerAction to pickTribeCards

--- src/main/java/it/polimi/ingsw/am17/Model/Game.java
+++ src/main/java/it/polimi/ingsw/am17/Model/Game.java
@@ -322,7 +322,7 @@ public class Game extends Subject {
      * @param characterCards TODO: check null
      * @param buildingCards TODO: check null
      */
-    public void playerAction(Player player, List<CharacterCard> characterCards, List<BuildingCard> buildingCards) {
+    public void pickTrib eCards(Player player, List<CharacterCard> characterCards, List<BuildingCard> buildingCards) {
         // Get leftmost occupied offering card.
         OfferingCard currentOffering = getNextOccupiedOfferingCard();
 
