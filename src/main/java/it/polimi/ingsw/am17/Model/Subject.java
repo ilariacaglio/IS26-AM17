297470be7f76264e85a24c4af1cec7903db3bde1
kerbless <kerbless@proton.me>
2026-04-11 16:47:18 +0200

    made attach/detach not abstract

--- src/main/java/it/polimi/ingsw/am17/Model/Subject.java
+++ src/main/java/it/polimi/ingsw/am17/Model/Subject.java
@@ -1,7 +1,7 @@
 package it.polimi.ingsw.am17.Model;
 
 public abstract class Subject {
-    abstract void attach(Observer observer);
-    abstract void detach(Observer observer);
+    void attach(Observer observer) {};
+    void detach(Observer observer) {};
     abstract void notifyObserver();
 }
