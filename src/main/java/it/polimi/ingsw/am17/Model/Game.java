9fd97f32168ca3a43d4e1997578fa7524eec12c4
ilariacaglio <ilaria.caglio@issgreppi.it>
2026-04-13 20:15:32 +0200

    Added method getId()

--- src/main/java/it/polimi/ingsw/am17/Model/Game.java
+++ src/main/java/it/polimi/ingsw/am17/Model/Game.java
@@ -66,10 +66,10 @@ public class Game extends Subject {
         };
     }
 
-    boolean isStarted() {
+    public boolean isStarted() {
         return currentEra > 0;
     }
-    boolean isEnded() {
+    public boolean isEnded() {
         return currentEra < 0;
     }
 
@@ -473,6 +473,9 @@ public class Game extends Subject {
         }
     }
 
+    public int getId(){
+        return id;
+    }
     /// only for testing
     protected List<Player> getPlayers(){
         return orderedPlayer;
