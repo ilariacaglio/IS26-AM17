0f78e0605e58d67ab83a414895bec32bf7054029
MarcoCR <crippa.marco04@gmail.com>
2026-04-13 03:29:44 +0200

    added BuildingType4Test

---
+++ src/test/java/it/polimi/ingsw/am17/Model/GameCard/BuildingType4Test.java
@@ -0,0 +1,52 @@
+package it.polimi.ingsw.am17.Model.GameCard;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BuildingType4Test extends BuildingCardTest {
+    BuildingType4 building;
+    Random r = new Random();
+    @BeforeEach
+    /**
+     * Generate a BuildingType4 with random value and stores the value of pointsFromEachCharacter
+     */
+    void setup()
+    {
+        building = new BuildingType4();
+    }
+
+    @Test
+    /**
+     * Run test of FinalPoints with different list of card generated randomly
+     * and check the return value is correct
+     */
+    void TestGetAdditionalFinalPoints()
+    {
+        for (int i = 0; i < 20; i++) {
+            int numHunter = r.nextInt(5);
+            int numBuilder = r.nextInt(5);
+            int numArtist = r.nextInt(5);
+            int numInv = r.nextInt(5);
+            int numSham = r.nextInt(5);
+            int numBind = r.nextInt(5);
+            List<CharacterCard> cc = createCharacterCardList(numHunter, numArtist, numBuilder, numInv, numSham, numBind);
+
+            int lowest = IntStream.of(numHunter, numBuilder, numArtist, numInv, numSham, numBind)
+                    .min()
+                    .getAsInt();
+
+            int fp = building.GetAdditionalFinalPoints(cc);
+
+            assertEquals(lowest*6, fp);
+        }
+
+
+    }
+}
