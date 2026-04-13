227242884dc0910490744fffefa7d6316d1a7a37
kerbless <kerbless@proton.me>
2026-04-13 22:57:12 +0200

    minimum attach implementation

--- src/main/java/it/polimi/ingsw/am17/Model/Subject.java
+++ src/main/java/it/polimi/ingsw/am17/Model/Subject.java
@@ -1,7 +1,26 @@
 package it.polimi.ingsw.am17.Model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Subject {
-    void attach(Observer observer) {};
-    void detach(Observer observer) {};
+    private final List<Observer> observers = new ArrayList<>();
+
+    /**
+     * Attach an observer to the subject (start observing).
+     * @param observer to be attached
+     */
+    void attach(Observer observer) {
+        observers.add(observer);
+    };
+
+
+    /**
+     * Do we need this? TODO
+     */
+    void detach(Observer observer) {
+
+    };
+
     abstract void notifyObserver();
 }
