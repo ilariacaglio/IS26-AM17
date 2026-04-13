8a8fcb63488d5372d9a3d43ceec436fa3b762268
MarcoCR <crippa.marco04@gmail.com>
2026-04-13 03:30:33 +0200

    added BuildingType6Test

---
+++ src/test/java/it/polimi/ingsw/am17/Model/GameCard/BuildingType6Test.java
@@ -0,0 +1,59 @@
+package it.polimi.ingsw.am17.Model.GameCard;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BuildingType6Test extends BuildingCardTest{
+    BuildingType6 building;
+    Random r = new Random();
+    @BeforeEach
+    /**
+     * Generate a BuildingType6 with random value and stores the value of pointsFromEachCharacter
+     */
+    void setup()
+    {
+        building = new BuildingType6();
+    }
+    @Test
+    /**
+     * Run test of FinalPoints with different list of card generated randomly
+     * and check the return value is correct
+     */
+    void TestGetAdditionalFinalPoints()
+    {
+        for (int i = 0; i < 5; i++) {
+            int numHunter = r.nextInt(5);
+            int numBuilder = r.nextInt(5);
+            int numArtist = r.nextInt(5);
+            int numInv = r.nextInt(5);
+            int numSham = r.nextInt(5);
+            int numBind = r.nextInt(5);
+            List<CharacterCard> cc = createCharacterCardList(numHunter, numArtist, numBuilder, numInv, numSham, numBind);
+            int fp = building.GetAdditionalFinalPoints(cc);
+
+            int sumPoints = sumOfBuilderPoints(cc);
+
+            assertEquals(sumPoints, fp);
+        }
+
+
+    }
+
+    private int sumOfBuilderPoints(List<CharacterCard> cc)
+    {
+        int sum=0;
+        for(CharacterCard c : cc)
+        {
+            if(c.getCardType() == CardType.BUILDER)
+            {
+                sum += ((Builder)c).getPointBonus();
+            }
+        }
+        return sum;
+    }
+}
