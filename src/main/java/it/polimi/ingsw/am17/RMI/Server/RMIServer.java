db611edfa69f568460630d6852509a1ddae8cc47
saracellegato <sara.cellegato@mail.polimi.it>
2026-04-18 17:28:49 +0200

    RMIServer override methods

--- src/main/java/it/polimi/ingsw/am17/RMI/Server/RMIServer.java
+++ src/main/java/it/polimi/ingsw/am17/RMI/Server/RMIServer.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.am17.RMI.Server;
 
 import it.polimi.ingsw.am17.Controller.GameController;
+import it.polimi.ingsw.am17.GameState;
 import it.polimi.ingsw.am17.Model.GameCard.BuildingCard;
 import it.polimi.ingsw.am17.Model.GameCard.CharacterCard;
 import it.polimi.ingsw.am17.Model.OfferingCard;
@@ -58,16 +59,49 @@ public class RMIServer extends UnicastRemoteObject implements VirtualServerRMI {
 
     @Override
     public void joinGame(UUID gameId, Player player) throws RemoteException {
+        System.err.println("joinGame request received");
+        this.controller.joinGame(gameId, player);
 
+        GameState currentState = new GameState();
+        //TODO: get real state of controller
+        //(GameState currentState = this.controller.getGameState(); )
+
+        synchronized (this.clients) {
+            for (VirtualViewRMI client : clients) {
+                client.showUpdate(currentState);
+
+            }
+        }
     }
 
     @Override
-    public void pickOfferingCard(UUID gameId, Player player, OfferingCard card) throws RemoteException {
+    public void pickOfferingCard(UUID gameId, Player player, OfferingCard card) throws RemoteException{
+        System.err.println("pickOfferingCard request received");
+        this.controller.pickOfferingCard(gameId, player, card);
 
-    }
+        GameState currentState = new GameState();
+        //TODO: same as joinGame()
+        synchronized (this.clients) {
+            for (VirtualViewRMI client : clients) {
+                client.showUpdate(currentState);
+            }
+        }
+        }
+
 
     @Override
     public void pickTribeCards(UUID gameId, Player player, List<CharacterCard> characterCards, List<BuildingCard> buildingCards) throws RemoteException {
+        System.err.println("pickTribeCards request received");
+        this.controller.pickTribeCards(gameId, player, characterCards, buildingCards);
 
+        GameState currentState = new GameState();
+        //TODO: same as joinGame()
+        synchronized (this.clients) {
+            for (VirtualViewRMI client : clients) {
+                client.showUpdate(currentState);
+            }
+        }
     }
+
+
 }
