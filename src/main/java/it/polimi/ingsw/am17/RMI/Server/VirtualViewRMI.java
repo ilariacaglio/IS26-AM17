8b2d2513be662d97b96d745230c71790d5b8e37d
ilariacaglio <ilaria.caglio@issgreppi.it>
2026-04-19 19:43:28 +0200

    added method in VirtualViewRMI

--- src/main/java/it/polimi/ingsw/am17/RMI/Server/VirtualViewRMI.java
+++ src/main/java/it/polimi/ingsw/am17/RMI/Server/VirtualViewRMI.java
@@ -3,6 +3,9 @@ package it.polimi.ingsw.am17.RMI.Server;
 import it.polimi.ingsw.am17.VirtualView;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.UUID;
 
 public interface VirtualViewRMI extends Remote, VirtualView {
+    void updateGameId(UUID gameId) throws RemoteException;
 }
