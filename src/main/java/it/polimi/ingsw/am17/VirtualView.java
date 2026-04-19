1f1c9296cb0bb635d2a2691a171ee9c63050ba9a
ilariacaglio <ilaria.caglio@issgreppi.it>
2026-04-19 18:11:38 +0200

    removed method and added comment

--- src/main/java/it/polimi/ingsw/am17/VirtualView.java
+++ src/main/java/it/polimi/ingsw/am17/VirtualView.java
@@ -5,6 +5,6 @@ import it.polimi.ingsw.am17.Model.Observer;
 import java.util.UUID;
 
 public interface VirtualView extends Observer {
+    // TODO: chiamato quando il client si registra come observer
     void updateGameId(UUID gameId) throws Exception;
-    void updatePlayerNumber(int playerNumber) throws Exception;
 }
