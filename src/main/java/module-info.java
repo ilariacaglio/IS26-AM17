ced7b3df49395c9e209c43916260c544091af9c5
MarcoCR <crippa.marco04@gmail.com>
2026-04-19 18:40:15 +0200

    added lines to module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,7 +6,11 @@ module it.polimi.ingsw.am17 {
     requires tools.jackson.databind;
     requires java.rmi;
 
+// Add this line to allow RMI to access your client interfaces
+    exports it.polimi.ingsw.am17.RMI.Client to java.rmi;
 
+    // If your Server-side RMI interfaces are in a different package, export that too
+    exports it.polimi.ingsw.am17.RMI.Server to java.rmi;
     opens it.polimi.ingsw.am17 to javafx.fxml;
     opens it.polimi.ingsw.am17.Model.GameCard to tools.jackson.databind;
     opens it.polimi.ingsw.am17.Model to tools.jackson.databind;
