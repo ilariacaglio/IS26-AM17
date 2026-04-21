6f7d531d407a18b6d7d58dd8d57993f166e1ef2a
ilariacaglio <ilaria.caglio@issgreppi.it>
2026-04-21 10:39:23 +0200

    Exception and RemoteException

--- src/main/java/it/polimi/ingsw/am17/RMI/Server/VirtualViewRMI.java
+++ src/main/java/it/polimi/ingsw/am17/RMI/Server/VirtualViewRMI.java
@@ -1,11 +1,22 @@
 package it.polimi.ingsw.am17.RMI.Server;
 
+import it.polimi.ingsw.am17.Model.GameCard.BuildingCard;
+import it.polimi.ingsw.am17.Model.GameCard.TribesCard;
+import it.polimi.ingsw.am17.Model.OfferingCard;
+import it.polimi.ingsw.am17.Model.Player;
 import it.polimi.ingsw.am17.VirtualView;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.List;
+import java.util.Stack;
 import java.util.UUID;
 
 public interface VirtualViewRMI extends Remote, VirtualView {
     void updateGameId(UUID gameId) throws RemoteException;
+    void updateEra(int era) throws RemoteException;
+    void updatePlayerStack(Stack<Player> orderedPlayer) throws RemoteException;
+    void updateOfferingCards(List<OfferingCard> offeringCards) throws RemoteException;
+    void updateTribesCards(List<TribesCard> upperRow, List<TribesCard> lowerRow) throws RemoteException;
+    void updateBuildingCards(List<BuildingCard> upperBuildingRow, List<BuildingCard> lowerBuildingRow) throws RemoteException;
 }
--- src/main/java/it/polimi/ingsw/am17/VirtualView.java
+++ src/main/java/it/polimi/ingsw/am17/VirtualView.java
@@ -1,9 +1,20 @@
 package it.polimi.ingsw.am17;
 
+import it.polimi.ingsw.am17.Model.GameCard.BuildingCard;
+import it.polimi.ingsw.am17.Model.GameCard.TribesCard;
 import it.polimi.ingsw.am17.Model.Observer;
+import it.polimi.ingsw.am17.Model.OfferingCard;
+import it.polimi.ingsw.am17.Model.Player;
 
+import java.util.List;
+import java.util.Stack;
 import java.util.UUID;
 
 public interface VirtualView extends Observer {
     void updateGameId(UUID gameId) throws Exception;
+    void updateEra(int era) throws Exception;
+    void updatePlayerStack(Stack<Player> orderedPlayer) throws Exception;
+    void updateOfferingCards(List<OfferingCard> offeringCards) throws Exception;
+    void updateTribesCards(List<TribesCard> upperRow, List<TribesCard> lowerRow) throws Exception;
+    void updateBuildingCards(List<BuildingCard> upperBuildingRow, List<BuildingCard> lowerBuildingRow) throws Exception;
 }
