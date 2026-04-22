d0237c10a3a88f615624565f77003952ae3aeff2
kerbless <kerbless@proton.me>
2026-04-22 17:28:35 +0200

    new files for socket

---
+++ src/main/java/it/polimi/ingsw/am17/Client/Socket/VirtualServerSocket.java
@@ -0,0 +1,4 @@
+package it.polimi.ingsw.am17.Client.Socket;
+
+public interface VirtualServerSocket {
+}
---
+++ src/main/java/it/polimi/ingsw/am17/Server/Socket/ServerSocket.java
@@ -0,0 +1,4 @@
+package it.polimi.ingsw.am17.Server.Socket;
+
+public class ServerSocket {
+}
