29900e485fa389f9c4169cbd74718a36b92bd641
kerbless <kerbless@proton.me>
2026-04-23 19:43:30 +0200

    correct socket class structure

--- src/main/java/it/polimi/ingsw/am17/Client/Socket/ClientSocket.java
+++ src/main/java/it/polimi/ingsw/am17/Client/Socket/ClientSocket.java
@@ -1,4 +1,49 @@
 package it.polimi.ingsw.am17.Client.Socket;
 
-public class ClientSocket {
+import it.polimi.ingsw.am17.CommonInterfaces.VirtualView;
+import it.polimi.ingsw.am17.Server.Model.GameCard.Buildings.BuildingCard;
+import it.polimi.ingsw.am17.Server.Model.GameCard.OfferingCard;
+import it.polimi.ingsw.am17.Server.Model.GameCard.TribeCards.TribesCard;
+import it.polimi.ingsw.am17.Server.Model.Player;
+
+import java.util.List;
+import java.util.Stack;
+import java.util.UUID;
+
+public class ClientSocket implements VirtualView {
+
+    @Override
+    public void updateGameId(UUID gameId) throws Exception {
+
+    }
+
+    @Override
+    public void updateGamesIdList(List<UUID> gamesIdList) throws Exception {
+
+    }
+
+    @Override
+    public void updateEra(int era) throws Exception {
+
+    }
+
+    @Override
+    public void updatePlayerStack(Stack<Player> orderedPlayer) throws Exception {
+
+    }
+
+    @Override
+    public void updateOfferingCards(List<OfferingCard> offeringCards) throws Exception {
+
+    }
+
+    @Override
+    public void updateTribesCards(List<TribesCard> upperRow, List<TribesCard> lowerRow) throws Exception {
+
+    }
+
+    @Override
+    public void updateBuildingCards(List<BuildingCard> upperBuildingRow, List<BuildingCard> lowerBuildingRow) throws Exception {
+
+    }
 }
--- src/main/java/it/polimi/ingsw/am17/Server/Socket/ServerSocket.java
+++ src/main/java/it/polimi/ingsw/am17/Server/Socket/ServerSocket.java
@@ -1,4 +1,39 @@
 package it.polimi.ingsw.am17.Server.Socket;
 
-public class ServerSocket {
+import it.polimi.ingsw.am17.CommonInterfaces.VirtualServer;
+import it.polimi.ingsw.am17.CommonInterfaces.VirtualView;
+import it.polimi.ingsw.am17.Server.Model.GameCard.Buildings.BuildingCard;
+import it.polimi.ingsw.am17.Server.Model.GameCard.OfferingCard;
+import it.polimi.ingsw.am17.Server.Model.GameCard.TribeCards.Characters.CharacterCard;
+import it.polimi.ingsw.am17.Server.Model.Player;
+
+import java.util.List;
+import java.util.UUID;
+
+public class ServerSocket implements VirtualServer {
+
+    @Override
+    public void getGamesList(VirtualView client) throws Exception {
+
+    }
+
+    @Override
+    public void createGame(VirtualView client, Player player, int numPlayers) throws Exception {
+
+    }
+
+    @Override
+    public void joinGame(VirtualView client, UUID gameId, Player player) throws Exception {
+
+    }
+
+    @Override
+    public void pickOfferingCard(UUID gameId, Player player, OfferingCard card) throws Exception {
+
+    }
+
+    @Override
+    public void pickTribeCards(UUID gameId, Player player, List<CharacterCard> characterCards, List<BuildingCard> buildingCards) throws Exception {
+
+    }
 }
