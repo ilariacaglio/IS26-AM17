49d692b8e89f5a574da7dcd4d9cc2a2b40ebc9bc
kerbless <kerbless@proton.me>
2026-04-23 20:30:34 +0200

    trying simple connection

--- src/main/java/it/polimi/ingsw/am17/Client/Socket/ClientSocket.java
+++ src/main/java/it/polimi/ingsw/am17/Client/Socket/ClientSocket.java
@@ -5,12 +5,21 @@ import it.polimi.ingsw.am17.Server.Model.GameCard.Buildings.BuildingCard;
 import it.polimi.ingsw.am17.Server.Model.GameCard.OfferingCard;
 import it.polimi.ingsw.am17.Server.Model.GameCard.TribeCards.TribesCard;
 import it.polimi.ingsw.am17.Server.Model.Player;
+import tools.jackson.databind.ObjectMapper;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
 import java.util.List;
 import java.util.Stack;
 import java.util.UUID;
 
 public class ClientSocket implements VirtualView {
+    VirtualServerSocket virtualServer;
+
+     public void start() throws IOException {
+        virtualServer = new VirtualServerSocket("localhost", 5000);
+    }
 
     @Override
     public void updateGameId(UUID gameId) throws Exception {
--- src/main/java/it/polimi/ingsw/am17/Client/Socket/VirtualServerSocket.java
+++ src/main/java/it/polimi/ingsw/am17/Client/Socket/VirtualServerSocket.java
@@ -6,15 +6,26 @@ import it.polimi.ingsw.am17.Server.Model.GameCard.Buildings.BuildingCard;
 import it.polimi.ingsw.am17.Server.Model.GameCard.OfferingCard;
 import it.polimi.ingsw.am17.Server.Model.GameCard.TribeCards.Characters.CharacterCard;
 import it.polimi.ingsw.am17.Server.Model.Player;
+import tools.jackson.databind.ObjectMapper;
 
+import java.io.IOException;
+import java.net.Socket;
 import java.util.List;
 import java.util.UUID;
 
 public class VirtualServerSocket implements VirtualServer {
+    Socket server;
+    ObjectMapper mapper;
+
+    public VirtualServerSocket(String host, int port) throws IOException {
+        server = new Socket(host, port);
+        mapper = new ObjectMapper();
+    }
+
 
     @Override
     public void getGamesList(VirtualView client) throws Exception {
-
+        mapper.writeValue(socket.getOutputStream(), msg);
     }
 
     @Override
