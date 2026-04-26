60dec59e1a7d9dc966a20b4b823e57c2a4617301
kerbless <kerbless@proton.me>
2026-04-26 18:46:31 +0200

    Updated messageType with new update methods

--- src/main/java/it/polimi/ingsw/am17/CommonInterfaces/MessageType.java
+++ src/main/java/it/polimi/ingsw/am17/CommonInterfaces/MessageType.java
@@ -1,17 +1,20 @@
 package it.polimi.ingsw.am17.CommonInterfaces;
 
 public enum MessageType {
+    // Client requests
     GET_GAMES_LIST,
     CREATE_GAME,
     JOIN_GAME,
     PICK_OFFERING_CARD,
     PICK_TRIBE_CARDS,
 
+    // Server sends updates
     UPDATE_GAME_ID,
     UPDATE_GAMES_ID_LIST,
     UPDATE_ERA,
     UPDATE_PLAYER_STACK,
-    UPDATE_OFFERING_CARDS,
-    UPDATE_TRIBES_CARDS,
-    UPDATE_BUILDING_CARDS
+    UPDATE_PLAYER_SELECT_OFFERING_CARD,
+    UPDATE_PLAYER_SELECT_TRIBE_CARDS,
+    UPDATE_END_TURN,
+    UPDATE_START_GAME,
 }
