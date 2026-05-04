b7553b294c87d10e8ff7c76be62ba46e1a21def8
MarcoCR04 <marco.crippa@issgreppi.it>
2026-05-05 00:01:27 +0200

    reworked Player.toString

--- src/main/java/it/polimi/ingsw/am17/Server/Model/Player.java
+++ src/main/java/it/polimi/ingsw/am17/Server/Model/Player.java
@@ -351,17 +351,19 @@ public class Player implements Serializable {
 
     @Override
     public String toString() {
-        String player = "Nickname: " + nickname +
+        String result = "Nickname: " + nickname +
                 "\nPp: " + pp +
                 "\nFood: " + food;
-        if(!characterCards.isEmpty() || !buildingCards.isEmpty())
-               player+= "\nCards: ";
-        for(CharacterCard c: this.characterCards){
-            player = player.concat(c.toString() +" ");
+        if(!characterCards.isEmpty() || !buildingCards.isEmpty()) {
+            result += "\nCharacter cards: ";
+            for (CharacterCard c : this.characterCards) {
+                result = result.concat("[" + c.toString() + "] ");
+            }
+            result += "\nBuilding cards: ";
+            for (BuildingCard c : this.buildingCards) {
+                result = result.concat("[" + c.toString() + "] ");
+            }
         }
-        for(BuildingCard c: this.buildingCards){
-            player = player.concat(c.toString() +" ");
-        }
-        return player;
+        return result;
     }
 }
