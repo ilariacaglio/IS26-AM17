0f6e93a9d56bd8a4c958bff22bcbea60f26ee4de
MarcoCR04 <marco.crippa@issgreppi.it>
2026-05-04 18:22:58 +0200

    add id as jackson parameter and manage when null

--- src/main/java/it/polimi/ingsw/am17/Server/Model/GameCard/TribeCards/TribesCard.java
+++ src/main/java/it/polimi/ingsw/am17/Server/Model/GameCard/TribeCards/TribesCard.java
@@ -19,10 +19,11 @@ public class TribesCard implements Serializable {
     }
 
     @JsonCreator
-    public TribesCard(@JsonProperty("era") int era, @JsonProperty("cardType") CardType cardType){
+    public TribesCard(@JsonProperty("era") int era, @JsonProperty("cardType") CardType cardType, @JsonProperty("id") UUID id){
         this.cardType=cardType;
         this.era=era;
-        this.id = UUID.randomUUID();
+        //If id is null generate a new one, otherwise use id.
+        this.id = (id == null) ? UUID.randomUUID() : id;
     }
 
     public UUID getId() {
