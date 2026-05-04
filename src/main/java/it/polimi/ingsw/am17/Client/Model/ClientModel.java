f2ab6c0d63e88a1f5061031685f5d8bc36c90d9c
MarcoCR04 <marco.crippa@issgreppi.it>
2026-05-04 19:18:28 +0200

    fix client model error from UnsupportedOperationException

--- src/main/java/it/polimi/ingsw/am17/Client/Model/ClientModel.java
+++ src/main/java/it/polimi/ingsw/am17/Client/Model/ClientModel.java
@@ -165,13 +165,8 @@ public class ClientModel {
     }
 
     public void setPlayerInStack(Player player){
-        for (int i = 0; i < orderedPlayer.size(); i++) {
-            if (orderedPlayer.get(i).equals(player)) {
-                orderedPlayer.set(i,player);
-                break;
-            }
-        }
-//        updateAllPlayers();
+        orderedPlayer.replaceAll(p -> p.equals(player) ? player : p);
+        updateAllPlayers();
     }
 
     public void removePlayerFromOfferingCard(Player player){
@@ -205,11 +200,10 @@ public class ClientModel {
     }
 
     public void updateAllPlayers() {
-        for(Player p : orderedPlayer) {
-            if(allPlayers.contains(p)){
-                int index = allPlayers.indexOf(p);
-                allPlayers.set(index, p);
-            }
+        this.allPlayers = new ArrayList<>(this.allPlayers);
+
+        for (Player p : orderedPlayer) {
+            allPlayers.replaceAll(existingPlayer -> existingPlayer.equals(p) ? p : existingPlayer);
         }
     }
 }
