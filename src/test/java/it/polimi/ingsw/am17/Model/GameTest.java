a20341290a8293d947434e94227acc28a52b5c34
ilariacaglio <ilaria.caglio@issgreppi.it>
2026-05-05 12:55:23 +0200

    fix testEndRound_ChangeEra_3 and testEndRound_EndGame

--- src/test/java/it/polimi/ingsw/am17/Model/GameTest.java
+++ src/test/java/it/polimi/ingsw/am17/Model/GameTest.java
@@ -195,7 +195,7 @@ public class GameTest {
         @Test
         void testEndRound_ChangeEra_3() {
             //play turns
-            for (int i = 0; i < 6; i++) {
+            for (int i = 0; i < 5; i++) {
                 game.endRound();
             }
             //get upper building row value
@@ -221,13 +221,13 @@ public class GameTest {
             assertEquals(2, game.getCurrentEra());
             game.endRound();
             game.endRound();
-            game.endRound();
             assertEquals(2, game.getCurrentEra());
             game.endRound();
             assertEquals(3, game.getCurrentEra());
             game.endRound();
             game.endRound();
             assertEquals(3, game.getCurrentEra());
+            game.endRound();
             game.endRound();
             //check if the game has ended
             assertEquals(-1, game.getCurrentEra());
