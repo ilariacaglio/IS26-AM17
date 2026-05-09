daab0a3787d40ff312b3882fd224e74e5e98515a
ilariacaglio <ilaria.caglio@issgreppi.it>
2026-05-09 14:31:39 +0200

    insertGameData method

--- src/main/java/it/polimi/ingsw/am17/Server/Utility/DatabaseManager.java
+++ src/main/java/it/polimi/ingsw/am17/Server/Utility/DatabaseManager.java
@@ -1,34 +1,38 @@
 package it.polimi.ingsw.am17.Server.Utility;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import it.polimi.ingsw.am17.Server.Model.Player;
 
 import java.sql.*;
 import java.time.LocalDate;
+import java.util.Queue;
 import java.util.UUID;
 import java.util.logging.Logger;
 
+// TODO: connection pool?
 public class DatabaseManager {
 
     // Loading file .env and connection data
-    private static final Dotenv dotenv = Dotenv.load();
+    private static final Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
     private static final String URL = dotenv.get("DB_URL");
     private static final String USERNAME = dotenv.get("DB_USER");
     private static final String PASSWORD = dotenv.get("DB_PASSWORD");
 
-    private final Logger logger = Logger.getLogger(DatabaseManager.class.getName());
+    private static final Logger logger = Logger.getLogger(DatabaseManager.class.getName());
 
     /**
-     * Inserts a new game into the game table
+     * Inserts a game into the game table
+     *
+     * @param gameId        the gameId to be inserted
+     * @param numPlayers    the numPlayers to be inserted
+     * @param connection     the open connection to db
      */
-    public void insertGame (UUID gameId, int numPlayers) {
+    private static void insertGame (UUID gameId, int numPlayers, Connection connection) throws SQLException {
         logger.info("Inserting game " + gameId + " with " + numPlayers + " players");
         // structure of the query
         String gameInsertQuery = "INSERT INTO Game(gameid, date, numplayers) VALUES(?, ?, ?)";
 
-        try (   // establish connection to database
-                Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-
-                // pre-compile the query
+        try (   // pre-compile the query
                 PreparedStatement statement = connection.prepareStatement(gameInsertQuery)) {
 
             // bind parameters to placeholders
@@ -38,26 +42,23 @@ public class DatabaseManager {
 
             // send the query to db
             statement.executeUpdate();
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
         }
     }
 
     /**
      * Inserts a player into the player table
+     *
      * @param nickname      the nickname to be inserted
      * @param gameId        the gameId to be inserted
      * @param finalPoints   the finalPoints to be inserted
+     * @param connection    the open connection to db
      */
-    public void insertPlayer (String nickname, UUID gameId, int finalPoints) {
+    private static void insertPlayer (String nickname, UUID gameId, int finalPoints, Connection connection) throws SQLException {
         logger.info("Inserting player " + nickname + " with " + finalPoints + " points into game " + gameId);
         // query structure
-        String playerInsertQuery = "INSERT INTO Player(nickname, gameid, finalpoints) VALUES(?, ?)";
-        try (   // establish connection to database
-                Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-
-                // pre-compile the query
-                PreparedStatement statement = connection.prepareStatement(playerInsertQuery)){
+        String playerInsertQuery = "INSERT INTO Player(nickname, gameid, finalpoints) VALUES(?, ?, ?)";
+        try (   // pre-compile the query
+                PreparedStatement statement = connection.prepareStatement(playerInsertQuery)) {
 
             // bind parameters to placeholders
             statement.setString(1, nickname);
@@ -66,8 +67,45 @@ public class DatabaseManager {
 
             // send the query to db
             statement.executeUpdate();
+        }
+    }
+
+    /**
+     * Inserts game data into db
+     *
+     * @param gameId        the id value of the game to be inserted
+     * @param numPlayers    the number of the players of the game to be inserted
+     * @param players       the players of the game to be inserted
+     */
+    public static void insertGameData (UUID gameId, int numPlayers, Queue<Player> players) {
+        logger.info("Inserting game data into game " + gameId);
+        try (   // establish connection to database
+                Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD)){
+
+            // begin transaction
+            connection.setAutoCommit(false);
+
+            try {
+                // insert game data
+                insertGame(gameId, numPlayers, connection);
+
+                // insert player data
+                for (Player player : players) {
+                    insertPlayer(player.getNickname(), gameId, player.getPp(), connection);
+                }
+
+                // commit changes
+                connection.commit();
+                logger.info("Successfully inserted game data into game " + gameId);
+            } catch (SQLException ex) {
+                // rollback changes
+                connection.rollback();
+                logger.severe("Transaction failed " + ex.getMessage());
+            } finally {
+                connection.setAutoCommit(true);
+            }
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            logger.severe(e.getMessage());
         }
     }
 }
