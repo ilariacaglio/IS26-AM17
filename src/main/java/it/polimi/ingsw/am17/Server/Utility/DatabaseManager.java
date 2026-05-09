91583ba6fc4fe4eac4368ad95ebd3618d00fe5b3
ilariacaglio <ilaria.caglio@issgreppi.it>
2026-05-09 14:02:45 +0200

    logging into DatabaseManager class

--- src/main/java/it/polimi/ingsw/am17/Server/Utility/DatabaseManager.java
+++ src/main/java/it/polimi/ingsw/am17/Server/Utility/DatabaseManager.java
@@ -1,9 +1,11 @@
 package it.polimi.ingsw.am17.Server.Utility;
 
 import io.github.cdimascio.dotenv.Dotenv;
+
 import java.sql.*;
 import java.time.LocalDate;
 import java.util.UUID;
+import java.util.logging.Logger;
 
 public class DatabaseManager {
 
@@ -13,10 +15,13 @@ public class DatabaseManager {
     private static final String USERNAME = dotenv.get("DB_USER");
     private static final String PASSWORD = dotenv.get("DB_PASSWORD");
 
+    private final Logger logger = Logger.getLogger(DatabaseManager.class.getName());
+
     /**
      * Inserts a new game into the game table
      */
     public void insertGame (UUID gameId, int numPlayers) {
+        logger.info("Inserting game " + gameId + " with " + numPlayers + " players");
         // structure of the query
         String gameInsertQuery = "INSERT INTO Game(gameid, date, numplayers) VALUES(?, ?, ?)";
 
@@ -24,7 +29,7 @@ public class DatabaseManager {
                 Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
 
                 // pre-compile the query
-                PreparedStatement statement = connection.prepareStatement(gameInsertQuery);) {
+                PreparedStatement statement = connection.prepareStatement(gameInsertQuery)) {
 
             // bind parameters to placeholders
             statement.setObject(1, gameId);
@@ -33,21 +38,26 @@ public class DatabaseManager {
 
             // send the query to db
             statement.executeUpdate();
-
-            // TODO: print logging
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
     }
 
+    /**
+     * Inserts a player into the player table
+     * @param nickname      the nickname to be inserted
+     * @param gameId        the gameId to be inserted
+     * @param finalPoints   the finalPoints to be inserted
+     */
     public void insertPlayer (String nickname, UUID gameId, int finalPoints) {
+        logger.info("Inserting player " + nickname + " with " + finalPoints + " points into game " + gameId);
         // query structure
         String playerInsertQuery = "INSERT INTO Player(nickname, gameid, finalpoints) VALUES(?, ?)";
         try (   // establish connection to database
                 Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
 
                 // pre-compile the query
-                PreparedStatement statement = connection.prepareStatement(playerInsertQuery);){
+                PreparedStatement statement = connection.prepareStatement(playerInsertQuery)){
 
             // bind parameters to placeholders
             statement.setString(1, nickname);
@@ -56,9 +66,8 @@ public class DatabaseManager {
 
             // send the query to db
             statement.executeUpdate();
-
         } catch (SQLException e) {
-
+            System.out.println(e.getMessage());
         }
     }
 }
