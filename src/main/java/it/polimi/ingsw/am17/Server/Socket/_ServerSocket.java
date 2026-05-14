73c020f8568f227b2614d58607837bbe3d7b4f43
kerbless <kerbless@proton.me>
2026-05-14 22:53:07 +0200

    close game case in socket server

--- src/main/java/it/polimi/ingsw/am17/Server/Socket/_ServerSocket.java
+++ src/main/java/it/polimi/ingsw/am17/Server/Socket/_ServerSocket.java
@@ -57,6 +57,7 @@ public class _ServerSocket implements Runnable, VirtualServer {
                     case GET_GAMES_LIST -> getGamesList(client);
                     case CREATE_GAME -> createGame(client, message.getPlayer(), message.getNumPlayers());
                     case JOIN_GAME -> joinGame(client, message.getGameId(), message.getPlayer());
+                    case CLOSE_GAME -> closeGame(client, message.getPlayer(), message.getGameId());
                     case PICK_OFFERING_CARD -> pickOfferingCard(message.getGameId(), message.getPlayer(), message.getOfferingCard());
                     case PICK_TRIBE_CARDS -> pickTribeCards(message.getGameId(), message.getPlayer(), message.getCharacterCards(), message.getBuildingCards());
                     case HEARTBEAT -> logger.finer("Received heartbeat");
