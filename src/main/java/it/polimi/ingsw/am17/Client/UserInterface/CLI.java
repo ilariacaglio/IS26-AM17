5fa75b8d1a1b42526f5c62b1c35828df817fe6ed
kerbless <kerbless@proton.me>
2026-05-14 22:53:55 +0200

    close game local state cleanup

--- src/main/java/it/polimi/ingsw/am17/Client/UserInterface/CLI.java
+++ src/main/java/it/polimi/ingsw/am17/Client/UserInterface/CLI.java
@@ -570,12 +570,20 @@ public class CLI implements UI {
     /**
      * Sends server command to close the current game
      */
-    private void closeGame(){
+    private void closeGame(Scanner scanner){
         try {
             if (game.getGameId() == null) {
                 System.out.print("Not in a game \n>");
             } else {
                 virtualServer.closeGame(client, game.getLocalPlayer(), game.getGameId());
+
+                // TODO: verify how to clean up local game state and keep nick/color
+                game = new ClientModel();
+                // Set up the user
+                String nickname = askNickname(scanner);
+                Color color = chooseColor(scanner);
+                game.createLocalPlayer(nickname, color);
+
             }
         }catch (Exception e) {
             System.err.println("CLI error: " + e.getMessage());
