d8e579307deff38d765463b7b043a7c2d2aa8b67
saracellegato <sara.cellegato@mail.polimi.it>
2026-05-19 14:22:40 +0200

    first implementation of global ranking

--- src/main/java/it/polimi/ingsw/am17/Client/UserInterface/GUI.java
+++ src/main/java/it/polimi/ingsw/am17/Client/UserInterface/GUI.java
@@ -10,6 +10,7 @@ import it.polimi.ingsw.am17.Server.Model.GameCard.TribeCards.Characters.Characte
 import it.polimi.ingsw.am17.Server.Model.GameCard.TribeCards.TribesCard;
 import it.polimi.ingsw.am17.Server.Model.Player;
 import it.polimi.ingsw.am17.Server.Utility.MoveValidator;
+import it.polimi.ingsw.am17.Server.Utility.RankingEntry;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.geometry.Insets;
@@ -407,14 +408,94 @@ public class GUI extends Application implements UI {
         return root;
     }
     private Parent globalRankingInterface(){
+        root = new VBox(20);
+        root.setPadding(new Insets(30));
+        root.setAlignment(Pos.TOP_CENTER);
+        //title
+        Label globalRankingTitle = new Label("\n--- YOUR POSITION IN GLOBAL RANKING ---");
+        globalRankingTitle.setStyle("""
+            -fx-text-fill: black;
+            -fx-font-size: 30px;
+            -fx-font-weight: bold;
+        """);
+        //ranking area
+        TextArea globalRankingArea = new TextArea();
 
-        VBox layout = new VBox();
+        globalRankingArea.setEditable(false);
+        globalRankingArea.setWrapText(true);
 
-        Label label = new Label("GLOBAL RANKING");
+        globalRankingArea.setStyle("""
+            -fx-font-size: 18px;
+            -fx-control-inner-background: #f4f4f4;
+            -fx-font-family: 'Consolas';
+        """);
+        //fill ranking
+        List<RankingEntry> globalRanking = staticGame.getRanking();
 
-        layout.getChildren().add(label);
+        StringBuilder globalRankingText = new StringBuilder();
 
-        return layout;
+        if (!globalRanking.isEmpty()) {
+            globalRankingText.append("--- YOUR POSITION IN GLOBAL RANKING ---\n\n");
+            RankingEntry userEntry = globalRanking.stream()
+                    .filter(e -> e.getGameId().equals(staticGame.getGameId())
+                            && e.getNickname().equals(localPlayer.getNickname()))
+                    .findFirst()
+                    .orElse(null);
+
+            if (userEntry != null) {
+                int pos = globalRanking.indexOf(userEntry) + 1;
+
+                globalRankingText.append(pos)
+                        .append(")\t")
+                        .append(userEntry.getNickname())
+                        .append("\t")
+                        .append(userEntry.getFinalPoints())
+                        .append("\n\n");
+            } else {
+                globalRankingText.append("Player data not found!\n\n");
+            }
+
+            // classifica globale completa
+            globalRankingText.append("--- GLOBAL RANKING ---\n");
+            globalRankingText.append("N.\tNICKNAME\t\tSCORE\n");
+
+            int rank = 1;
+
+            for (RankingEntry entry : globalRanking) {
+
+                globalRankingText.append(rank)
+                        .append(")\t")
+                        .append(entry.getNickname())
+                        .append("\t\t")
+                        .append(entry.getFinalPoints())
+                        .append("\n");
+
+                rank++;
+            }
+        }
+
+        globalRankingArea.setText(globalRankingText.toString());
+
+        VBox.setVgrow(globalRankingArea, Priority.ALWAYS);
+
+
+        //button to global ranking interface
+        Button end = new Button("END");
+            end.setStyle("""
+            -fx-font-size: 18px;
+            -fx-font-weight: bold;
+            -fx-padding: 10px 20px;
+        """);
+        //close game
+        end.setOnAction(e -> {
+            Platform.exit();
+            System.exit(0);
+        });
+
+        root.getChildren().addAll(globalRankingTitle, globalRankingArea, end);
+
+
+        return root;
 
     }
 
@@ -425,7 +506,7 @@ public class GUI extends Application implements UI {
         //title
         Label rankingTitle = new Label("--- FINAL GAME RANKING ---");
         rankingTitle.setStyle("""
-            -fx-text-fill: white;
+            -fx-text-fill: black;
             -fx-font-size: 30px;
             -fx-font-weight: bold;
         """);
