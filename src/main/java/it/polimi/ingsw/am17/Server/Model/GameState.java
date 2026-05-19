5b605544c9d6e003eb9d6a16801108257ba2861b
ilariacaglio <ilaria.caglio@issgreppi.it>
2026-05-19 13:32:54 +0200

    era in game class

--- src/main/java/it/polimi/ingsw/am17/Server/Model/GameState.java
+++ src/main/java/it/polimi/ingsw/am17/Server/Model/GameState.java
@@ -6,5 +6,19 @@ public enum GameState {
     ERA1,
     ERA2,
     ERA3,
-    ENDED
+    ENDED;
+
+    /**
+     * @return true if game is in an era, false otherwise
+     */
+    public boolean isGameStarted() {
+        return this.equals(GameState.ERA1) || this.equals(GameState.ERA2) || this.equals(GameState.ERA3);
+    }
+
+    /**
+     * @return true if game is ended, false otherwise
+     */
+    public boolean isGameEnded() {
+        return this.equals(GameState.ENDED);
+    }
 }
