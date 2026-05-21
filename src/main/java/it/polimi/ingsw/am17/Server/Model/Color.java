819780d9433664f0a26e80546332c02e05d1b3b0
saracellegato <sara.cellegato@mail.polimi.it>
2026-05-21 17:08:53 +0200

    changed offering card color when selected by player totem white

--- src/main/java/it/polimi/ingsw/am17/Server/Model/Color.java
+++ src/main/java/it/polimi/ingsw/am17/Server/Model/Color.java
@@ -11,7 +11,7 @@ public enum Color {
         return switch (this) {
             case RED    -> javafx.scene.paint.Color.RED;
             case BLUE   -> javafx.scene.paint.Color.BLUE;
-            case WHITE  -> javafx.scene.paint.Color.WHITE;
+            case WHITE  -> javafx.scene.paint.Color.GRAY;
             case BLACK  -> javafx.scene.paint.Color.BLACK;
             case YELLOW -> javafx.scene.paint.Color.GOLD; // Gold  looks better
         };
