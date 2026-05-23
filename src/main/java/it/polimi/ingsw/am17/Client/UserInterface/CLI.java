7e895dc83477161972bb5e699f5c29c061f5fee0
ilariacaglio <ilaria.caglio@issgreppi.it>
2026-05-23 11:36:22 +0200

    deleted spacing

--- src/main/java/it/polimi/ingsw/am17/Client/UserInterface/CLI.java
+++ src/main/java/it/polimi/ingsw/am17/Client/UserInterface/CLI.java
@@ -189,10 +189,6 @@ public class CLI implements UI {
             //clear console
             System.out.print("\033[H\033[2J\033[3J");
             System.out.flush();
-            // TODO: remove this loop for real terminal execution
-            for (int i = 0; i < 50; i++) {
-                System.out.println();
-            }
 
             if(errorMessage != null && !errorMessage.isBlank()) {
                 System.out.println(ANSI_RED+errorMessage+ANSI_RESET);
