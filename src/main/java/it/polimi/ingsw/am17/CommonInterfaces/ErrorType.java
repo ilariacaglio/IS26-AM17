8274fb2bb05e6dde8e60440cdf631584de097225
ilariacaglio <ilaria.caglio@issgreppi.it>
2026-05-23 19:11:34 +0200

    error enum

---
+++ src/main/java/it/polimi/ingsw/am17/CommonInterfaces/ErrorType.java
@@ -0,0 +1,38 @@
+package it.polimi.ingsw.am17.CommonInterfaces;
+
+public enum ErrorType {
+    // handling game and players
+    INVALID_NUMBER_OF_PLAYERS ("Wrong number of players!"),
+    GAME_ALREADY_STARTED("The game has already started!"),
+    DUPLICATE_NICKNAME("The game has already a player with the same nickname!"),
+    DUPLICATE_COLOR("The game has already a player with the same color!"),
+    INVALID_GAME_STATE("Invalid game state"),
+
+    // offering card selection
+    MISSING_OFFERING_CARD_LETTER ("offeringCardLetter can't be null!"),
+    INVALID_OFFERING_CARD_LETTER("Illegal card selection. (Card not found)"),
+    UNAVAILABLE_OFFERING_CARD("Illegal card selection. (Card already selected by another player)"),
+    OFFERING_CARD_ALREADY_SELECTED("Illegal card selection. (Offering Card already selected)"),
+
+    // game moves
+    OUT_OF_TURN ("It is not the player's turn!"),
+    INSUFFICIENT_FOOD("Not enough food to buy building cards"),
+
+    // default
+    UNKNOWN("There was an error handling your request");
+
+    private final String message;
+
+    ErrorType(final String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public String toString() {
+        return this.name();
+    }
+}
