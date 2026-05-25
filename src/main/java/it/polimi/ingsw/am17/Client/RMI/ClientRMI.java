9a94d2741fb0fdacc88e1b7987981f2b61db091a
ilariacaglio <ilaria.caglio@issgreppi.it>
2026-05-25 16:56:58 +0200

    removed unused import

--- src/main/java/it/polimi/ingsw/am17/Client/RMI/ClientRMI.java
+++ src/main/java/it/polimi/ingsw/am17/Client/RMI/ClientRMI.java
@@ -23,7 +23,6 @@ import java.util.*;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
