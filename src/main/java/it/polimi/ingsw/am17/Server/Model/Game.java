d199f66e02b658d43cfafb72055ac00de09b771a
Ilaria Caglio <91674882+ilariacaglio@users.noreply.github.com>
2026-05-25 16:55:39 +0200

    Update src/main/java/it/polimi/ingsw/am17/Server/Model/Game.java
    Co-authored-by: kerbless <kerbless@protonmail.com>

--- src/main/java/it/polimi/ingsw/am17/Server/Model/Game.java
+++ src/main/java/it/polimi/ingsw/am17/Server/Model/Game.java
@@ -141,7 +141,7 @@ public class Game extends Subject {
     public void forceEndGame(String nickname) {
         logger.severe("Forcibly closing game with id: " + id);
         this.gameState = GameState.ENDED;
-        notifyEndGame(nickname, null, null);
+        notifyEndGame(nickname, null, null); // no ranking and ... when game closed early
     }
 
     /**
