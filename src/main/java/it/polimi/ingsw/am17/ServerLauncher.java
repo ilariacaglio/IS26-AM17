b1e835d9ed7db1daf187a470866d3d8e47bf994b
kerbless <kerbless@proton.me>
2026-05-26 13:09:03 +0200

    new command line options

--- src/main/java/it/polimi/ingsw/am17/ServerLauncher.java
+++ src/main/java/it/polimi/ingsw/am17/ServerLauncher.java
@@ -4,6 +4,8 @@ import it.polimi.ingsw.am17.Server.Controller.GamesController;
 import it.polimi.ingsw.am17.Server.RMI.ServerRMI;
 import it.polimi.ingsw.am17.Server.Socket.ServerSocketMultiplexer;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Logger;
 
 /**
@@ -12,14 +14,47 @@ import java.util.logging.Logger;
 public class ServerLauncher {
     private static final Logger logger = Logger.getLogger(ServerLauncher.class.getName());
 
+    /**
+     * Launches a server.
+     * Use --serverName to specify the server name for RMI.
+     * Use --portRMI to specify the port for RMI.
+     * Use --portSocket to specify the port for Socket.
+     * Use --debug to raise logging level. TODO
+     */
     static void main(String[] args) {
+        List<String> argsList = Arrays.asList(args);
+
+        String serverName;
+        if (argsList.contains("--serverName")) {
+            serverName = argsList.get(argsList.indexOf("--serverName") + 1);
+        }
+        else {
+            serverName = "MesosRMIServer";
+        }
+
+        int portRMI;
+        if (argsList.contains("--portRMI")) {
+            portRMI = Integer.parseInt(argsList.get(argsList.indexOf("--portRMI") + 1));
+        }
+        else {
+            portRMI = 1099;
+        }
+
+        int portSocket;
+        if (argsList.contains("--portSocket")) {
+            portSocket = Integer.parseInt(argsList.get(argsList.indexOf("--portSocket") + 1));
+        }
+        else {
+            portSocket = 24312;
+        }
+
         // create the main controller
         GamesController controller = new GamesController();
 
         // Start an RMI server in its own thread
         new Thread(() -> {
             try {
-                new ServerRMI(controller, 1099, "MesosRMIServer");
+                new ServerRMI(controller, portRMI, serverName);
             } catch (Exception e) {
                 logger.severe("RMI server failed to start: " + e.getMessage());
             }
@@ -28,7 +63,7 @@ public class ServerLauncher {
         // Start a Socket server in its own thread
         new Thread(() -> {
             try {
-                new ServerSocketMultiplexer(controller, 5000);
+                new ServerSocketMultiplexer(controller, portSocket);
             } catch (Exception e) {
                 logger.severe("Socket server failed to start: " + e.getMessage());
             }
