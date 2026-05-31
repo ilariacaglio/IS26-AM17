32fafe6c2735f93768da9b18d445e403f5245402
MarcoCR04 <crippa.marco04@gmail.com>
2026-05-31 16:15:39 +0200

    fix merge

--- src/main/java/it/polimi/ingsw/am17/Client/UserInterface/GUIElements/GameView.java
+++ src/main/java/it/polimi/ingsw/am17/Client/UserInterface/GUIElements/GameView.java
@@ -3,6 +3,8 @@ package it.polimi.ingsw.am17.Client.UserInterface.GUIElements;
 import it.polimi.ingsw.am17.Client.Model.ClientModel;
 import it.polimi.ingsw.am17.Client.UserInterface.CardGUI;
 import it.polimi.ingsw.am17.Client.UserInterface.GUI;
+import it.polimi.ingsw.am17.Client.UserInterface.TurnCardGUI;
+import it.polimi.ingsw.am17.CommonInterfaces.InvalidOperationException;
 import it.polimi.ingsw.am17.Server.Model.Color;
 import it.polimi.ingsw.am17.Server.Model.GameCard.Buildings.BuildingCard;
 import it.polimi.ingsw.am17.Server.Model.GameCard.OfferingCard;
