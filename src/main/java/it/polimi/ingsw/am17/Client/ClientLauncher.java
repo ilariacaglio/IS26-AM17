feabc4aa7721b07c5f39edf3d2e2917edc606ec7
ilariacaglio <ilaria.caglio@issgreppi.it>
2026-06-12 17:36:08 +0200

    logging level

--- src/main/java/it/polimi/ingsw/am17/Client/ClientLauncher.java
+++ src/main/java/it/polimi/ingsw/am17/Client/ClientLauncher.java
@@ -7,13 +7,17 @@ import java.io.IOException;
 import java.rmi.NotBoundException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Launches a client.
  * Use --host to specify the host.
  * Use --portRMI to specify the port for RMI.
  * Use --portSocket to specify the port for Socket.
- * Use --debug to raise logging level. TODO
+ * Use --debug to raise logging level.
  * Use --socket to use socket communication (instead or RMI).
  * Use --gui to launch a graphical interface.
  */
@@ -24,6 +28,22 @@ public class ClientLauncher {
 
         boolean gui = argsList.contains("--gui");
         boolean socket = argsList.contains("--socket");
+        boolean debug = argsList.contains("--debug");
+
+        // logging configuration
+        Logger rootLogger = Logger.getLogger("");
+        // set default level higher than INFO to disable log printing
+        Level targetLevel = debug ? Level.ALL : Level.WARNING;
+        rootLogger.setLevel(targetLevel);
+        // set console handler logging level
+        for (Handler handler : rootLogger.getHandlers()) {
+            if (handler instanceof ConsoleHandler) {
+                handler.setLevel(Level.ALL);
+            }
+        }
+        if (debug) {
+            Logger.getLogger(ClientLauncher.class.getName()).info("Debug mode enabled: logs will be printed");
+        }
 
         String host;
         if (argsList.contains("--host")) {
