d17c5f9759989ed71ba7fcba24bc2b2d23d72c4c
ilariacaglio <ilaria.caglio@issgreppi.it>
2026-06-15 19:10:29 +0200

    GameView javadoc and solved warnings

--- src/main/java/it/polimi/ingsw/am17/Client/UserInterface/GUIElements/GameView.java
+++ src/main/java/it/polimi/ingsw/am17/Client/UserInterface/GUIElements/GameView.java
@@ -3,7 +3,6 @@ package it.polimi.ingsw.am17.Client.UserInterface.GUIElements;
 import it.polimi.ingsw.am17.Client.Model.ClientModel;
 import it.polimi.ingsw.am17.Client.UserInterface.CardGUI;
 import it.polimi.ingsw.am17.Client.UserInterface.GUI;
-import it.polimi.ingsw.am17.Client.UserInterface.GUIElements.TurnCardGUI;
 import it.polimi.ingsw.am17.CommonInterfaces.InvalidOperationException;
 import it.polimi.ingsw.am17.Server.Model.Color;
 import it.polimi.ingsw.am17.Server.Model.GameCard.Buildings.BuildingCard;
@@ -27,37 +26,38 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
+/**
+ * Draws the interface displayed when game is started.
+ */
 public class GameView {
     private VBox root;
     private final GUI mainGui;
     private VBox turnOverlay;
     private HBox upperCardsBox;
     private HBox lowerCardsBox;
-    private final ClientModel game;
-    private final Player localPlayer;
+    private HBox playerCardsBox;
+    private HBox playerResourcesBox;
     private final List<CardGUI> offeringCardGUI = new ArrayList<>();
     private TurnCardGUI turnCard;
+    private final ClientModel readOnlyModel;
+    private final Player localPlayer;
+    private Player selectedPlayer;
     private List<CharacterCard> tribesSelected = new ArrayList<>();
     private List<BuildingCard> buildingSelected = new ArrayList<>();
     private OfferingCard offeringSelected = null;
-    private Player selectedPlayer;
-    private HBox playerCardsBox;
-    private HBox playerResourcesBox;
-    private Label name;
-    private Label food;
-    private Label points;
 
-
-    public GameView(GUI mainGui, ClientModel game, Player localPlayer) {
+    public GameView(GUI mainGui, ClientModel readOnlyModel, Player localPlayer) {
         this.mainGui = mainGui;
-        this.game = game;
+        this.readOnlyModel = readOnlyModel;
         this.localPlayer = localPlayer;
         this.selectedPlayer = localPlayer;
         buildUI();
     }
 
+    // todo: this method is 250 lines long, split!!
     private void buildUI() {
         //set fullscreen size
+        // TODO: this doesn't always work
         Stage stage = (Stage) mainGui.scene.getWindow();
         stage.setMaximized(true);
 
@@ -89,7 +89,7 @@ public class GameView {
 
         turnOverlay.getChildren().add(turnText);
         turnOverlay.setVisible(false);
-        if(game.isPlayerTurn(localPlayer)){
+        if(readOnlyModel.isPlayerTurn(localPlayer)){
             turnOverlay.setVisible(true);
         }
 
@@ -99,19 +99,19 @@ public class GameView {
             throw new RuntimeException("Could not find image at /images/background_game.png inside resources!");
         }
 
-// 2. Create the Image object
+        // 2. Create the Image object
         Image image = new Image(imageUrl.toExternalForm());
 
-// 3. Define the background settings (Equivalent to your CSS)
+        // 3. Define the background settings
         BackgroundImage bgImage = new BackgroundImage(
                 image,
                 BackgroundRepeat.NO_REPEAT, // -fx-background-repeat: no-repeat
                 BackgroundRepeat.NO_REPEAT,
-                BackgroundPosition.CENTER,  // -fx-background-position: center center
+                BackgroundPosition.CENTER,  // -fx-background-position: center
                 new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, true) // -fx-background-size: cover
         );
 
-// 4. Apply it to your root Pane
+        // 4. Apply it to your root Pane
         root.setBackground(new Background(bgImage));
 
         Label localPlayerName = new Label("Local Player: " + localPlayer.getNickname());
@@ -132,11 +132,11 @@ public class GameView {
         //put turnCard and offeringCard in the same HBox
         HBox offeringCardBox = new HBox(20);
         //turnCard first
-        turnCard = new TurnCardGUI(game.getTURN_CARD_IMAGE_PATH(), game.getNumPlayers());
+        turnCard = new TurnCardGUI(readOnlyModel.getTURN_CARD_IMAGE_PATH(), readOnlyModel.getNumPlayers());
         offeringCardBox.getChildren().add(turnCard);
 
         //offeringCards second
-        for(OfferingCard card : game.getOfferingCards()){
+        for(OfferingCard card : readOnlyModel.getOfferingCards()){
             CardGUI offeringCard;
             if(card.getPlayer() == null) {
                 offeringCard = new CardGUI(card.getImagePath());
@@ -156,12 +156,12 @@ public class GameView {
 
         //send button
         Button sendButton = new Button("SEND");
-        sendButton.setOnAction(e ->
+        sendButton.setOnAction(_ ->
         {
             try {
                 if (offeringSelected != null) {
                     try{
-                        game.validateOfferingCardTurnAction(localPlayer, offeringSelected.getOrderLetter());
+                        readOnlyModel.validateOfferingCardTurnAction(localPlayer, offeringSelected.getOrderLetter());
                         mainGui.pickOfferingCard(offeringSelected);
                         offeringSelected = null;
                         buildingSelected = new ArrayList<>();
@@ -181,13 +181,8 @@ public class GameView {
                     }
 
                 } else {
-                    // get players offering card
-                    OfferingCard myOfferingCard = game.getOfferingCards().stream()
-                            .filter(c->c.getPlayer()!= null && c.getPlayer().equals(localPlayer))
-                            .findFirst().orElse(null);
-
                     try {
-                        game.validateTribeCardsTurnAction(localPlayer, tribesSelected, buildingSelected);
+                        readOnlyModel.validateTribeCardsTurnAction(localPlayer, tribesSelected, buildingSelected);
                         mainGui.pickTribeCards(tribesSelected, buildingSelected);
                         offeringSelected = null;
                         buildingSelected = new ArrayList<>();
@@ -229,7 +224,7 @@ public class GameView {
         HBox showCardsBox = new HBox(10);
         ScrollPane otherScrollPane = new ScrollPane(showCardsBox);
         //final Player[] openedPlayer = {null};
-        for(Player p : game.getOrderedPlayers()){
+        for(Player p : readOnlyModel.getOrderedPlayers()){
             Button playerButton = new Button(p.getNickname() );
             //set nickname color
             Color nicknameColor = p.getColor();
@@ -269,7 +264,7 @@ public class GameView {
 
             playersCardsBox.getChildren().add(playerButton);
             StackPane.setAlignment(playersCardsBox, Pos.BOTTOM_CENTER);
-            playerButton.setOnAction(e -> {
+            playerButton.setOnAction(_ -> {
                 selectedPlayer = p;
                 createPlayerCardLabel();
                 updateSelectedPlayer();
@@ -329,7 +324,7 @@ public class GameView {
      */
     public void updatePlayerQueue(){
         // Update turn overlay visibility
-        turnOverlay.setVisible(game.isPlayerTurn(localPlayer));
+        turnOverlay.setVisible(readOnlyModel.isPlayerTurn(localPlayer));
     }
 
     /**
@@ -337,14 +332,14 @@ public class GameView {
      */
     public void updateGameElements() {
         // Update turn overlay visibility
-        turnOverlay.setVisible(game.isPlayerTurn(localPlayer));
+        turnOverlay.setVisible(readOnlyModel.isPlayerTurn(localPlayer));
 
 
         // Update Upper Cards
         updateUpperCards();
 
         //UpdateTurnCards
-        List<Color> currentTurnOrder = game.getOrderedPlayers().stream()
+        List<Color> currentTurnOrder = readOnlyModel.getOrderedPlayers().stream()
                 .map(Player::getColor)
                 .toList();
 
@@ -366,15 +361,15 @@ public class GameView {
      */
     public void updateGameCardDecks() {
         // Update turn overlay visibility
-        turnOverlay.setVisible(game.isPlayerTurn(localPlayer));
+        turnOverlay.setVisible(readOnlyModel.isPlayerTurn(localPlayer));
 
         // Update Upper Cards
         upperCardsBox.getChildren().removeIf(node -> {
             Object cardData = node.getUserData();
 
             // Check if the card still exists in the game model
-            boolean stillInTribeRow = game.getUpperTribeRow().contains(cardData);
-            boolean stillInBuildingRow = game.getUpperBuildingRow().contains(cardData);
+            boolean stillInTribeRow = readOnlyModel.getUpperTribeRow().contains(cardData);
+            boolean stillInBuildingRow = readOnlyModel.getUpperBuildingRow().contains(cardData);
 
             // If it is NOT in the tribe row AND NOT in the building row, remove it (return true)
             return !stillInTribeRow && !stillInBuildingRow;
@@ -385,8 +380,8 @@ public class GameView {
             Object cardData = node.getUserData();
 
             // Check if the card still exists in the game model
-            boolean stillInTribeRow = game.getLowerTribeRow().contains(cardData);
-            boolean stillInBuildingRow = game.getLowerBuildingRow().contains(cardData);
+            boolean stillInTribeRow = readOnlyModel.getLowerTribeRow().contains(cardData);
+            boolean stillInBuildingRow = readOnlyModel.getLowerBuildingRow().contains(cardData);
 
             // If it is NOT in the tribe row AND NOT in the building row, remove it
             return !stillInTribeRow && !stillInBuildingRow;
@@ -402,7 +397,7 @@ public class GameView {
      */
     public void updateOfferingDeck(){
         // Update turn overlay visibility
-        turnOverlay.setVisible(game.isPlayerTurn(localPlayer));
+        turnOverlay.setVisible(readOnlyModel.isPlayerTurn(localPlayer));
 
         for (CardGUI cardGUI : offeringCardGUI){
             if(cardGUI.isSelected())
@@ -410,7 +405,7 @@ public class GameView {
         }
         for(int i=0; i<offeringCardGUI.size(); i++){
             CardGUI card = offeringCardGUI.get(i);
-            Player p = game.getOfferingCards().get(i).getPlayer();
+            Player p = readOnlyModel.getOfferingCards().get(i).getPlayer();
             if (p != null) {
                 card.updateTotem(p.getColor().getFxColor());
                 turnCard.removePlayerTotem(p.getColor());
@@ -426,13 +421,13 @@ public class GameView {
      */
     private void updateUpperCards() {
         upperCardsBox.getChildren().clear(); // Remove old cards
-        for(TribesCard card : game.getUpperTribeRow()){
+        for(TribesCard card : readOnlyModel.getUpperTribeRow()){
             CardGUI upperCard = new CardGUI(card.getImagePath());
             upperCard.setUserData(card);
             setOnMouseClickForTribes(upperCard, card);
             upperCardsBox.getChildren().add(upperCard);
         }
-        for(BuildingCard card : game.getUpperBuildingRow()){
+        for(BuildingCard card : readOnlyModel.getUpperBuildingRow()){
             CardGUI upperCard = new CardGUI(card.getImagePath());
             upperCard.setUserData(card);
             setOnMouseClickForBuilding(upperCard, card);
@@ -445,13 +440,13 @@ public class GameView {
      */
     private void updateLowerCards(){
         lowerCardsBox.getChildren().clear();
-        for(TribesCard card : game.getLowerTribeRow()){
+        for(TribesCard card : readOnlyModel.getLowerTribeRow()){
             CardGUI lowerCard = new CardGUI(card.getImagePath());
             lowerCard.setUserData(card);
             setOnMouseClickForTribes(lowerCard, card);
             lowerCardsBox.getChildren().add(lowerCard);
         }
-        for(BuildingCard card : game.getLowerBuildingRow()){
+        for(BuildingCard card : readOnlyModel.getLowerBuildingRow()){
             CardGUI lowerCard = new CardGUI(card.getImagePath());
             lowerCard.setUserData(card);
             setOnMouseClickForBuilding(lowerCard, card);
@@ -460,6 +455,9 @@ public class GameView {
     }
 
 
+    /**
+     * Resets offering card colored border and updates totem placement
+     */
     private void updateOfferingCards(){
 
         //set all card to not selected
@@ -472,8 +470,8 @@ public class GameView {
         for (int i = 0; i < offeringCardGUI.size(); i++) {
             CardGUI card = offeringCardGUI.get(i);
 
-            if (i < game.getOfferingCards().size()) {
-                OfferingCard serverCard = game.getOfferingCards().get(i);
+            if (i < readOnlyModel.getOfferingCards().size()) {
+                OfferingCard serverCard = readOnlyModel.getOfferingCards().get(i);
 
                 card.setUserData(serverCard);
 
@@ -489,18 +487,16 @@ public class GameView {
 
     /**
      * set the action from click on tribes card
-     * @param cardGUI
-     * @param card
      */
     private void setOnMouseClickForTribes(CardGUI cardGUI, TribesCard card) {
         if(card.getCardType().isCharacter()) {
-            cardGUI.setOnMouseClicked(event -> {
-                if (!game.isPlayerTurn(localPlayer)) {
+            cardGUI.setOnMouseClicked(_ -> {
+                if (!readOnlyModel.isPlayerTurn(localPlayer)) {
                     showWaitTurnAlert();
                     return;
                 }
 
-                if (game.isPickOCPhase()) {
+                if (readOnlyModel.isPickOCPhase()) {
                     return; // Not selectable right now
                 }
 
@@ -514,17 +510,15 @@ public class GameView {
 
     /**
      * set the action from click on building card
-     * @param cardGUI
-     * @param card
      */
     private void setOnMouseClickForBuilding(CardGUI cardGUI, BuildingCard card ) {
-        cardGUI.setOnMouseClicked(event -> {
-            if (!game.isPlayerTurn(localPlayer)) {
+        cardGUI.setOnMouseClicked(_ -> {
+            if (!readOnlyModel.isPlayerTurn(localPlayer)) {
                 showWaitTurnAlert(); // The main GUI handles the alert, not the card!
                 return;
             }
 
-            if (game.isPickOCPhase()) {
+            if (readOnlyModel.isPickOCPhase()) {
                 return; // Not selectable right now
             }
 
@@ -538,21 +532,19 @@ public class GameView {
 
     /**
      * set the action from click on offering card
-     * @param cardGUI
-     * @param card
      */
     private void setOnMouseClickForOffering(CardGUI cardGUI, OfferingCard card) {
-        cardGUI.setOnMouseClicked(event -> {
-            if (!game.isPlayerTurn(localPlayer)) {
+        cardGUI.setOnMouseClicked(_ -> {
+            if (!readOnlyModel.isPlayerTurn(localPlayer)) {
                 showWaitTurnAlert(); // The main GUI handles the alert, not the card!
                 return;
             }
 
-            if (!game.isPickOCPhase()) {
+            if (!readOnlyModel.isPickOCPhase()) {
                 return; // Not selectable right now
             }
 
-            OfferingCard thisCard = game.getOfferingCards().stream()
+            OfferingCard thisCard = readOnlyModel.getOfferingCards().stream()
                     .filter(c -> c.equals(card)) // Put your condition inside filter()
                     .findFirst()
                     .orElse(null);
@@ -568,6 +560,9 @@ public class GameView {
         });
     }
 
+    /**
+     * Displays an alert when the user plays out of its turn
+     */
     private void showWaitTurnAlert(){
         Alert alert = new Alert(Alert.AlertType.WARNING);
         alert.setTitle("Wait Your Turn");
@@ -577,6 +572,9 @@ public class GameView {
 
     }
 
+    /**
+     * Handles tribes card selection by adding or removing the card to it
+     */
     public void tribesSelected(CharacterCard card) {
         if(tribesSelected.contains(card))
             tribesSelected.remove(card);
@@ -584,6 +582,9 @@ public class GameView {
             tribesSelected.add(card);
     }
 
+    /**
+     * Handles building card selection by adding or removing the card to it
+     */
     public void buildingSelected(BuildingCard card) {
         if(buildingSelected.contains(card))
             buildingSelected.remove(card);
@@ -591,6 +592,9 @@ public class GameView {
             buildingSelected.add(card);
     }
 
+    /**
+     * Handles offering card selection
+     */
     public void offeringSelected(OfferingCard card) {
         offeringSelected = card;
 
@@ -605,7 +609,7 @@ public class GameView {
      */
     private void updateSelectedPlayer(){
         //update selected player
-        selectedPlayer = game.findPlayer(selectedPlayer);
+        selectedPlayer = readOnlyModel.findPlayer(selectedPlayer);
 
         List<CharacterCard> orderedCards = new ArrayList<>(
                 selectedPlayer.getCharacterCards()
@@ -627,8 +631,11 @@ public class GameView {
         }
     }
 
+    /**
+     * Displays the selected players data (excluding cards)
+     */
     private void createPlayerCardLabel(){
-        selectedPlayer = game.findPlayer(selectedPlayer);
+        selectedPlayer = readOnlyModel.findPlayer(selectedPlayer);
         Label name = new Label("Name: " + selectedPlayer.getNickname());
         name.setStyle("""
             -fx-text-fill: white;
